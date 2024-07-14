92a6f144bd3f6de5cea3ab430b97b14d7268813a
Hillel Coren <hillelcoren@gmail.com>
2024-07-14 09:27:02 +0300

    Added breeze

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: "192.168.10.10",
+        },
+        cors: {
+            origin: '*',
+        },
+        host: "192.168.10.10",
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
