996797bfa3a5168522f0f2016b122e9d2e4a1746
Hillel Coren <hillelcoren@gmail.com>
2024-07-14 23:53:54 +0300

    Roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,8 +2,24 @@
 
 namespace App\Models;
 
-class Role
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
 {
+    protected $fillable = [
+        'type',
+        'name',
+        'phone',
+        'email',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'postal_code',
+        'country_code',
+        'description',
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class);
