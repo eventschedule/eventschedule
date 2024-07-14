4958d874241bb18099f2fce386db90de5087766c
Hillel Coren <hillelcoren@gmail.com>
2024-07-14 14:42:48 +0300

    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,8 +90,8 @@
                 </div>
             </div>
             <div class="text-center">
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">*****.eventschedule.com</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Enabling venues, vendors and talent to collaborate with ease</p>
+                <h1 class="text-6xl font-bold tracking-tight text-gray-900 sm:text-6xl">*****.eventschedule.com</h1>
+                <p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">Enabling venues, vendors and talent to collaborate with ease</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
                 <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
