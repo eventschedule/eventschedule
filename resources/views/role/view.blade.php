1780cee75f87ae2998bbb04d1346f10804acf570
Hillel Coren <hillelcoren@gmail.com>
2024-07-15 22:10:17 +0300

    Roles

--- resources/views/role/view.blade.php
+++ resources/views/role/view.blade.php
@@ -17,14 +17,8 @@
                 <ol role="list" class="flex items-center space-x-4">
                     <li>
                         <div class="flex items-center">
-                            <svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                                    clip-rule="evenodd" />
-                            </svg>
                             <a href="#"
-                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Engineering</a>
+                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{{ __(ucwords($role->type)) }}</a>
                         </div>
                     </li>
                     <li>
@@ -36,8 +30,7 @@
                                     clip-rule="evenodd" />
                             </svg>
                             <a href="#" aria-current="page"
-                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Back End
-                                Developer</a>
+                                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{{ $role->name }}</a>
                         </div>
                     </li>
                 </ol>
@@ -45,8 +38,7 @@
         </div>
         <div class="mt-2 md:flex md:items-center md:justify-between">
             <div class="min-w-0 flex-1">
-                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Back
-                    End Developer</h2>
+                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">{{ $role->name }}</h2>
             </div>
             <div class="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
 
