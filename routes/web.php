352266273d0b325ac4a5ffe6f3561b7c372e2e59
Hillel Coren <hillelcoren@gmail.com>
2024-07-15 14:41:24 +0300

    Roles

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/sign_up', [RoleController::class, 'create'])->name('role.create');
     Route::post('/store', [RoleController::class, 'store'])->name('role.store');
     
+    Route::get('/venues', [RoleController::class, 'viewVenues'])->name('role.view_venues');
+    Route::get('/talent', [RoleController::class, 'viewTalents'])->name('role.view_talents');
+    Route::get('/vendors', [RoleController::class, 'viewVendors'])->name('role.view_vendors');
+
     Route::get('/venue/{venue}', [RoleController::class, 'viewVenue'])->name('role.view_venue');
+    Route::get('/talent/{talent}', [RoleController::class, 'viewTalent'])->name('role.view_talent');
+    Route::get('/vendor/{vendor}', [RoleController::class, 'viewVendor'])->name('role.view_vendor');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
