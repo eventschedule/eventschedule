f141b77466fbf790212bd8e08df21f059907827d
Hillel Coren <hillelcoren@gmail.com>
2024-07-16 17:01:46 +0300

    Roles

--- resources/views/role/show_admin.blade.php
+++ resources/views/role/show_admin.blade.php
@@ -60,7 +60,7 @@
                 </span>
 
                 <span class="ml-3 hidden sm:block">
-                    <a href="{{ url('/view/' . $role->subodmain) }}" target="_blank">
+                    <a href="{{ url('/view/' . $role->subdomain) }}" target="_blank">
                         <button type="button"
                             class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                             <svg class="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
