d84ecd3a18400290f1cc2b20a227074dce99dd3c
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 22:12:58 +0300

    Roles

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,13 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.css";
+
+// Example: Initialize Flatpickr on elements with the class 'datepicker'
+document.addEventListener('DOMContentLoaded', function () {
+    flatpickr('.datepicker');
+});
