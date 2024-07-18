8cc5fae3b494efcf43fb071e5032a1e8df4ba4ee
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 18:53:53 +0300

    Roles

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -7,6 +7,7 @@
         @method('put')
         @endif
 
+
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
@@ -36,11 +37,7 @@
 
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="pt-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                     <div class="max-w-xl">
 
