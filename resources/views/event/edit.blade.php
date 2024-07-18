79e370d350263f4ea0b6447ed2ef89e76f287886
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 18:46:08 +0300

    Roles

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -39,6 +39,68 @@
             </div>
         </div>
 
+        <div class="pt-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
+                    <div class="max-w-xl">
+
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
+                            {{ __('Details') }}
+                        </h2>
+
+                        @if($talent)
+                        <div>{{ $talent->name }}</div>
+                        @elseif($vendor)
+                        <div>{{ $vendor->name }}</div>
+                        @else
+
+                        <fieldset>
+                            <x-input-label for="type" :value="__('Type')" />
+                            <div class="mt-2 mb-6 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                                <div class="flex items-center">
+                                    <input id="talent" name="type" type="radio" value="talent"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="talent"
+                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">Talent</label>
+                                </div>
+                                <div class="flex items-center">
+                                    <input id="vendor" name="type" type="radio" value="vendor"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="vendor"
+                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">Vendor</label>
+                                </div>
+                            </div>
+                        </fieldset>
+
+
+                        <div class="mb-6">
+                            <x-input-label for="role_name" :value="__('Name')" />
+                            <x-text-input id="role_name" name="role_name" type="text" class="mt-1 block w-full"
+                                :value="old('role_name')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('role_name')" />
+                        </div>
+
+                        <div class="mb-6">
+                            <x-input-label for="role_email" :value="__('Email')" />
+                            <x-text-input id="role_email" name="role_email" type="email" class="mt-1 block w-full"
+                                :value="old('role_email')" required />
+                            <x-input-error class="mt-2" :messages="$errors->get('role_email')" />
+                        </div>
+
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="flex items-center gap-4">
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+            </div>
+        </div>
+
     </form>
 
 </x-app-layout>
