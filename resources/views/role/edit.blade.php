4d12ccc4edaba2413424410885a90f467782f900
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 18:46:08 +0300

    Roles

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -183,12 +183,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="flex items-center gap-4">
                 <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-                @if (session('status') === 'role-saved')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
-                @endif
-
             </div>
         </div>
 
