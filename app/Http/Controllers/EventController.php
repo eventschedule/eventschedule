ad9b8404f67d00983bdfa2f0478094b823d06ac1
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 18:25:38 +0300

    Roles

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,15 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\Role;
 
 class EventController extends Controller
 {
-    public function create()
+    public function create($subdomain1, $subdomain2 = '')
     {
+        $role1 = Role::subdomain($subdomain1)->firstOrFail();
+        $role2 = $subdomain2 ? Role::subdomain($subdomain2)->firstOrFail() : false;
+
+        $venue = false;
+        $vendor = false;
+        $talent = false;
+
+        if ($role1->isVenue()) {
+            $venue = $role1;
+        } else if ($role2 && $role2->isVenue()) {
+            $venue = $role2;
+        }
+
+        if ($role1->isVendor()) {
+            $vendor = $role1;
+        } else if ($role2 && $role2->isVendor()) {
+            $vendor = $role2;
+        }
+
+        if ($role1->isTalent()) {
+            $talent = $role1;
+        } else if ($role2 && $role2->isTalent()) {
+            $talent = $role2;
+        }
+        
+        if (! auth()->user()->hasRole($venue->subdomain)) {
+            return redirect('/');
+        }
+
         $event = new Event;
 
         $data = [
             'event' => $event,
+            'venue' => $venue,
+            'talent' => $talent,
+            'vendor' => $vendor,
         ];
 
         return view('event/edit', $data);
--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -13,8 +13,27 @@
                     <div class="max-w-xl">
 
                         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
-                            {{ __('Details') }}
+                            {{ __('Venue') }}
                         </h2>
+
+                        @if($venue)
+                        <div>{{ $venue->name }}</div>
+                        @else
+                        <div class="mb-6">
+                            <x-input-label for="venue_name" :value="__('Name')" />
+                            <x-text-input id="venue_name" name="venue_name" type="text" class="mt-1 block w-full"
+                                :value="old('venue_name')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('venue_name')" />
+                        </div>
+
+                        <div class="mb-6">
+                            <x-input-label for="venue_email" :value="__('Email')" />
+                            <x-text-input id="venue_email" name="venue_email" type="email" class="mt-1 block w-full"
+                                :value="old('venue_email')" required />
+                            <x-input-error class="mt-2" :messages="$errors->get('venue_name')" />
+                        </div>
+                        @endif
+
                     </div>
                 </div>
             </div>
