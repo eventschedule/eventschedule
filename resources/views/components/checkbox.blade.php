c88ac66c87aafd15962b1c33c9abe8019314ed97
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 21:55:52 +0300

    Roles

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,7 @@
 <div class="relative flex items-start">
     <div class="flex h-6 items-center">
-        <input id="{{ $name }}" name="{{ $name }}" type="checkbox" {{ $checked ? 'checked' : '' }}
+        <input type="hidden" name="{{ $name }}" value="0">
+        <input id="{{ $name }}" name="{{ $name }}" type="checkbox" value="1" {{ $checked ? 'checked' : '' }}
             class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
     </div>
     <div class="ml-3 text-sm leading-6">
