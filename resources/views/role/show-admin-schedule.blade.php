a89d097d3da190f23262f3ddd8b3bbb5f3daf1c7
Hillel Coren <hillelcoren@gmail.com>
2024-07-18 15:12:53 +0300

    Roles

---
+++ resources/views/role/show-admin-schedule.blade.php
@@ -0,0 +1,19 @@
+<div class="text-center pt-10">
+    <svg class="mx-auto h-12 w-12 text-gray-400" fill="#ccc" viewBox="0 0 24 24" stroke="currentColor"
+        aria-hidden="true">
+        <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1" />
+    </svg>
+    <h3 class="mt-2 text-sm font-semibold text-gray-900">{{ __('No events') }}</h3>
+    <p class="mt-1 text-sm text-gray-500">{{ __('Get started by creating a new event') }}</p>
+    <div class="mt-6">
+        <button type="button"
+            class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path
+                    d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+            </svg>
+            {{ __('New Event') }}
+        </button>
+    </div>
+</div>
