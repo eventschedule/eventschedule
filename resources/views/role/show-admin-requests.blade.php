bee234d01096863375599ae4462bd9136eec2c3e
Hillel Coren <hillelcoren@gmail.com>
2024-07-20 23:39:04 +0300

    Events

--- resources/views/role/show-admin-requests.blade.php
+++ resources/views/role/show-admin-requests.blade.php
@@ -1,12 +1,13 @@
 <ul role="list" class="divide-y divide-gray-100">
-  <li class="relative flex justify-between gap-x-6 py-5 bg-white">
+  @foreach($events as $event)
+  <li class="relative flex justify-between gap-x-6 px-5 py-5 bg-white">
     <div class="flex min-w-0 gap-x-4">
       <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
       <div class="min-w-0 flex-auto">
         <p class="text-sm font-semibold leading-6 text-gray-900">
           <a href="#">
             <span class="absolute inset-x-0 -top-px bottom-0"></span>
-            Leslie Alexander
+            {{ $event->role->name }}
           </a>
         </p>
         <p class="mt-1 flex text-xs leading-5 text-gray-500">
@@ -24,4 +25,5 @@
       </svg>
     </div>
   </li>
+  @endforeach
 </ul>
