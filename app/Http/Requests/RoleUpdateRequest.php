c8bb7432a18018d3ca1e08700139d79e3ba97a0c
Hillel Coren <hillelcoren@gmail.com>
2024-07-21 23:28:37 +0300

    Events

--- app/Http/Requests/RoleUpdateRequest.php
+++ app/Http/Requests/RoleUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -17,7 +18,7 @@ class RoleUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             //'subdomain' => ['required', 'string', 'lowercase', 'max:255', Rule::unique(Role::class)->ignore($this->route('role'),
         ];
     }
