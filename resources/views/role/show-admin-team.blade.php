bd7d88d869b1f6896f7375b9b681ff0e43c3afe1
Hillel Coren <hillelcoren@gmail.com>
2024-07-21 19:40:49 +0300

    Events

--- resources/views/role/show-admin-team.blade.php
+++ resources/views/role/show-admin-team.blade.php
@@ -6,9 +6,17 @@
             email and role.</p>
         -->
     </div>
-    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button"
-            class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __('Add Member') }}</button>
+    <div class="mt-6 sm:ml-16 sm:mt-0 sm:flex-none">
+        <a href="{{ route('event.create', ['subdomain1' => $role->subdomain]) }}">
+            <button type="button"
+                class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path
+                        d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                </svg>
+                {{ __('Add Member') }}
+            </button>
+        </a>
     </div>
 </div>
 <div class="mt-8 flow-root">
@@ -19,8 +27,10 @@
                     <tr>
                         <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
                             {{ __('Name') }}</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">{{ __('Email') }}</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">{{ __('Role') }}</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                            {{ __('Email') }}</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                            {{ __('Role') }}</th>
                         <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0"></th>
                     </tr>
                 </thead>
@@ -31,7 +41,7 @@
                             {{ $user->name }}</td>
                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><a
                                 href="mailto:{{ $user->email }}">{{ $user->email }}</td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $user->pivot->level }}</td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ __(ucwords($user->pivot->level)) }}</td>
                         <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                             <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Remove') }}</a>
                         </td>
