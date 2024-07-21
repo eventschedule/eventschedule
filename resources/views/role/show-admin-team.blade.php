91efcc14aed826bdf873cc915a63c1cf9637ac01
Hillel Coren <hillelcoren@gmail.com>
2024-07-21 20:42:01 +0300

    Events

--- resources/views/role/show-admin-team.blade.php
+++ resources/views/role/show-admin-team.blade.php
@@ -20,35 +20,44 @@
     </div>
 </div>
 <div class="mt-8 flow-root">
-    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="min-w-full divide-y divide-gray-300">
-                <thead>
-                    <tr>
-                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                            {{ __('Name') }}</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                            {{ __('Email') }}</th>
-                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                            {{ __('Role') }}</th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0"></th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200">
-                    @foreach ($members as $member)
-                    <tr class="bg-white">
-                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                            {{ $member->name }}</td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><a
-                                href="mailto:{{ $member->email }}">{{ $member->email }}</td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ __(ucwords($member->pivot->level)) }}</td>
-                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Remove') }}</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="overflow-x-auto px-4 sm:px-6 lg:px-8 bg-white">
+        <div class="inline-block min-w-full py-2 align-middle">
+            <div class="overflow-hidden min-w-full">
+                <table class="min-w-full divide-y divide-gray-300">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                {{ __('Name') }}
+                            </th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                {{ __('Email') }}
+                            </th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                {{ __('Role') }}
+                            </th>
+                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 bg-white">
+                        @foreach ($members as $member)
+                        <tr class="bg-white">
+                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                {{ $member->name }}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                <a href="mailto:{{ $member->email }}">{{ $member->email }}</a>
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {{ __(ucwords($member->pivot->level)) }}
+                            </td>
+                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Remove') }}</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
