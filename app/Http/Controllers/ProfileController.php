a29fdb8359d3ea2f3fc203b1dba1ad8669d61960
Hillel Coren <hillelcoren@gmail.com>
2024-07-22 12:08:10 +0300

    Events

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -37,7 +37,7 @@ class ProfileController extends Controller
 
         if ($request->hasFile('profile_image')) {
             $file = $request->file('profile_image');
-            $filename = 'profile_' . Str::random(10) . '_' . time() . '.' . $file->getClientOriginalExtension();            
+            $filename = strtolower('profile_' . Str::random(10) . '_' . time() . '.' . $file->getClientOriginalExtension());
             $path = $file->storeAs('images', $filename, 'public');
             
             $user = $request->user();
