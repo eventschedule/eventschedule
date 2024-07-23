de494d89f5b87f4fa41c6bfbb0ce1cf2fe7bbf14
Hillel Coren <hillelcoren@gmail.com>
2024-07-23 09:47:11 +0300

    Events

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -221,10 +221,10 @@ class RoleController extends Controller
     {
         $role = new Role;
         $role->background = 'default';
+        $role->background_color = '#FFFFFF';
         $role->background_colors = '#7468E6, #C44B85';
         $role->background_rotation = 135;
         $role->font_color = '#000000';
-        $role->background_color = '#FFFFFF';
         $role->accent_color = '#007BFF';
         $role->accept_talent_requests = true;
 
