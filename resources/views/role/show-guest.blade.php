a2982dd3314a48f7437954f3e55e36d6de07effa
Hillel Coren <hillelcoren@gmail.com>
2024-07-23 18:16:02 +0300

    Events

--- resources/views/role/show-guest.blade.php
+++ resources/views/role/show-guest.blade.php
@@ -49,16 +49,28 @@
                 @endif
             </div>
             <div>
+                @if ($role->isVenue())
                 <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode($role->fullAddress()) }}" target="_blank">
                     <button type="button" style="background-color: #ccc"
                         class="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                        <svg class="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path
+                                    d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+                            </svg>
                         {{ __('Map') }}
                     </button>
                 </a>
+                @endif
 
                 <a href="" class="pl-4">
                     <button type="button" style="background-color: {{ $role->accent_color }}"
                         class="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                        <svg class="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path
+                                    d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+                            </svg>
                         {{ __('Follow') }}
                     </button>
                 </a>
