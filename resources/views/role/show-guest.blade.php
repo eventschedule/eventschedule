7b09fbf0ded6acd3f047677350c5d15c36ed76ba
Hillel Coren <hillelcoren@gmail.com>
2024-07-23 16:44:58 +0300

    Events

--- resources/views/role/show-guest.blade.php
+++ resources/views/role/show-guest.blade.php
@@ -38,7 +38,7 @@
 <body class="antialiased dark:bg-black dark:text-white/50">
 
     <div class="p-10 max-w-4xl mx-auto px-4">
-        <div class="flex items-center justify-between pb-6">
+        <div class="flex items-start justify-between pb-6">
             <div>
                 @if ($role->profile_image_url)
                 <img src="{{ $role->profile_image_url }}" style="max-height:100px" />
@@ -61,7 +61,30 @@
         @include('role/partials/calendar', ['showAdd' => false, 'route' => 'guest'])
 
         <div class="py-6">
-            {{ $role->description }}
+            <div class="flex items-start justify-between pb-6">
+                <div>
+                    {{ $role->description }}
+                </div>
+                <div>
+                @if ($role->accept_talent_requests)
+                <a href="">
+                    <button type="button" style="background-color: {{ $role->accent_color }}"
+                        class="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                        {{ __('Sign up to Perform') }}
+                    </button>
+                </a>
+                @endif
+
+                @if ($role->accept_vendor_requests)
+                <a href="">
+                    <button type="button" style="background-color: {{ $role->accent_color }}"
+                        class="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                        {{ __('Sign up to Vend') }}
+                    </button>
+                </a>
+                @endif
+                </div>
+            </div>
         </div>
 
         @if ($role->youtube_links)
