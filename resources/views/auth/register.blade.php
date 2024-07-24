44de6833942e818f448dd99e2e792c53d9171ed1
Hillel Coren <hillelcoren@gmail.com>
2024-07-24 17:23:33 +0300

    Events

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-auth-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -63,4 +63,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-auth-layout>
