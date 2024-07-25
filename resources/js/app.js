0cd29bae322c5ccdd16f63ae9dab81f2a7281a8f
Hillel Coren <hillelcoren@gmail.com>
2024-07-25 13:45:35 +0300

    Events

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,5 +6,20 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.css';
+
+import EasyMDE from 'easymde';
+import 'easymde/dist/easymde.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const easyMDE = new EasyMDE({ 
+        element: document.getElementById('description'),
+        toolbar: [
+            'bold', 'italic', 'heading', '|',
+            'quote', 'unordered-list', 'ordered-list', '|',
+            'link', 'image', '|',
+            'preview', 'guide'
+        ],
+    });
+});
