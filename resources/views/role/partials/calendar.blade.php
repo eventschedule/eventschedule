eca70ab8d2367e86f05df2aac3cfddb965833ec4
Hillel Coren <hillelcoren@gmail.com>
2024-07-26 11:17:38 +0300

    Events

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -51,7 +51,7 @@
                 -->
                 @if ($showAdd)
                 <div class="ml-3 h-6 w-px bg-gray-300"></div>
-                <a href="{{ route('event.create', ['subdomain1' => $role->subdomain]) }}">
+                <a href="{{ route('event.create', ['subdomain' => $role->subdomain]) }}">
                     <button type="button"
                         class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
