a7fb1690b246be9e8187a172d67cc751ff5b31eb
Hillel Coren <hillelcoren@gmail.com>
2024-07-26 11:22:24 +0300

    Events

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -119,7 +119,7 @@
                 @while ($currentDate->lte($endOfMonth))
                 @if ($showAdd)
                     <div class="cursor-pointer relative {{ $currentDate->month == $month ? 'bg-white hover:bg-gray-100 hover:border-gray-300' : 'bg-gray-50 text-gray-500' }} px-3 py-2 min-h-[100px] border-1 border-transparent hover:border-gray-300"
-                        onclick="window.location = '{{ route('event.create', ['subdomain1' => $role->subdomain, 'date' => $currentDate->format('Y-m-d')]) }}';">
+                        onclick="window.location = '{{ route('event.create', ['subdomain' => $role->subdomain, 'date' => $currentDate->format('Y-m-d')]) }}';">
                 @else
                 <div class="relative {{ $currentDate->month == $month ? 'bg-white' : 'bg-gray-50 text-gray-500' }} px-3 py-2 min-h-[100px] border-1 border-transparent">
                 @endif
