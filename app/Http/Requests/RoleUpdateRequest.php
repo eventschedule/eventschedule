d6c0046698d4a1423b35639d30507e4582ea348d
Hillel Coren <hillelcoren@gmail.com>
2024-07-28 23:40:56 +0300

    Events

--- app/Http/Requests/RoleUpdateRequest.php
+++ app/Http/Requests/RoleUpdateRequest.php
@@ -18,7 +18,7 @@ class RoleUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique(Role::class)->ignore($this->route('subdomain'), 'subdomain')],
+            'email' => ['required', 'string', 'email', 'max:255'],
             //'subdomain' => ['required', 'string', 'max:255', Rule::unique(Role::class)->ignore($this->route('subdomain'))],
             'profile_image' => ['image', 'max:2500'],
             'background_image_url' => ['image', 'max:2500'],
