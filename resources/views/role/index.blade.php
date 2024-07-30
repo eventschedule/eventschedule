7ed3213bc0d3d70b9868f7183ae3079e9d27c200
Hillel Coren <hillelcoren@gmail.com>
2024-07-30 21:53:43 +0300

    Events

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -27,7 +27,8 @@
                             @foreach ($roles as $role)
                             <tr class="bg-white">
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                    <a href="{{ route('role.view_guest', ['subdomain' => $role->subdomain]) }}" target="_blank">{{ $role->name }}</a>
+                                    <a href="{{ route('role.view_guest', ['subdomain' => $role->subdomain]) }}"
+                                        target="_blank">{{ $role->name }}</a>
                                 </td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                     <a href="mailto:{{ $role->email }}">{{ $role->email }}</a>
@@ -40,7 +41,11 @@
                                         target="_blank">{{ App\Utils\UrlUtils::clean($role->website) }}</a>
                                 </td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    <a href="{{ route('role.unfollow', ['subdomain' => $role->subdomain]) }}">{{ __('messages.unfollow') }}</a>
+                                    <button type="button"
+                                        onclick="var confirmed = confirm('{{ __('messages.are_you_sure') }}'); if (confirmed) { location.href = '{{ route('role.unfollow', ['subdomain' => $role->subdomain]) }}'; return false; }"
+                                        class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                        {{ __('messages.unfollow') }}
+                                    </button>
                                 </td>
                             </tr>
                             @endforeach
