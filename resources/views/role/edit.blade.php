11a297a6aaec8a3112c9289c75dfb3c476690b6b
Hillel Coren <hillelcoren@gmail.com>
2024-07-30 14:58:57 +0300

    Events

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -189,15 +189,6 @@
                             <x-input-error class="mt-2" :messages="$errors->get('name')" />
                         </div>
 
-                        @if ($role->exists)
-                        <div class="mb-6">
-                            <x-input-label for="subdomain" :value="__('messages.subdomain')" />
-                            <x-text-input id="subdomain" name="subdomain" type="text" class="mt-1 block w-full"
-                                :value="old('subdomain', $role->subdomain)" required />
-                            <x-input-error class="mt-2" :messages="$errors->get('subdomain')" />
-                        </div>
-                        @endif
-
                         <div class="mb-6">
                             <x-input-label for="description" :value="__('messages.description')" />
                             <textarea id="description" name="description"
@@ -423,6 +414,15 @@
                         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
                             {{ __('messages.settings') }}
                         </h2>
+
+                        @if ($role->exists)
+                        <div class="mb-6">
+                            <x-input-label for="subdomain" :value="__('messages.subdomain')" />
+                            <x-text-input id="subdomain" name="subdomain" type="text" class="mt-1 block w-full"
+                                :value="old('subdomain', $role->subdomain)" required />
+                            <x-input-error class="mt-2" :messages="$errors->get('subdomain')" />
+                        </div>
+                        @endif
 
                         <div class="mb-6">
                             <x-input-label for="visibility" :value="__('messages.visibility')" />
