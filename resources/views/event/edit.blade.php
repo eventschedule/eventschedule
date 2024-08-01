ad46af3deef471d0b84ba3d32fcb3c4737caabbd
Hillel Coren <hillelcoren@gmail.com>
2024-08-01 19:02:23 +0300

    Events

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -177,7 +177,8 @@
                         </div>
                         @else
                         <div class="mb-6">
-                            <select name="role_id">
+                            <select name="role_id"
+                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">>
                                 @foreach ($roles as $role)
                                 <option value="{{ App\Utils\UrlUtils::encodeId($role->id) }}">{{ $role->name }}</option>
                                 @endforeach
