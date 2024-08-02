adaff43f244d68344b97dcd800f52ce20a052674
Hillel Coren <hillelcoren@gmail.com>
2024-08-02 13:22:48 +0300

    Events

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -10,6 +10,7 @@
         // Example: Initialize Flatpickr on elements with the class 'datepicker'
         document.addEventListener('DOMContentLoaded', function() {
             flatpickr('.datepicker', {
+                allowInput: true,
                 enableTime: true,
                 altInput: true,
                 time_24hr: "{{ $venue && $venue->use_24_hour_time ? 'true' : 'false' }}",
@@ -78,6 +79,128 @@
                     </div>
                 </div>
 
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
+                    <div class="max-w-xl">
+
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
+                            @if($venue && ! $venue->accept_talent_requests)
+                            {{ __('messages.vendor') }}
+                            @elseif($venue && ! $venue->accept_vendor_requests)
+                            {{ __('messages.talent') }}
+                            @else
+                            {{ __('messages.talent_vendor') }}
+                            @endif
+                        </h2>
+
+
+                        @if($venue && auth()->user()->isMember($venue->subdomain))
+                        @if($venue->accept_talent_requests && $venue->accept_vendor_requests)
+                        <fieldset>
+                            <x-input-label for="role_type" :value="__('messages.type')" />
+                            <div class="mt-2 mb-6 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                                <div class="flex items-center">
+                                    <input id="talent" name="role_type" type="radio" value="talent" CHECKED
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="talent"
+                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ __('messages.talent') }}</label>
+                                </div>
+                                <div class="flex items-center">
+                                    <input id="vendor" name="role_type" type="radio" value="vendor"
+                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                    <label for="vendor"
+                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ __('messages.vendor') }}</label>
+                                </div>
+                            </div>
+                        </fieldset>
+                        @elseif($venue->accept_talent_requests || $talent)
+                        <input type="hidden" name="role_type" value="talent" />
+                        @elseif($venue->accept_vendor_requests || $vendor)
+                        <input type="hidden" name="role_type" value="vendor" />
+                        @endif
+                        @endif
+
+                        @if ($talent)
+                        <div class="mb-6">
+                            <a href="{{ route('role.view_guest', ['subdomain' => $talent->subdomain]) }}"
+                                target="_blank">
+                                {{ $talent->name }}
+                            </a>
+                        </div>
+                        @elseif ($vendor)
+                        <div class="mb-6">
+                            <a href="{{ route('role.view_guest', ['subdomain' => $vendor->subdomain]) }}"
+                                target="_blank">
+                                {{ $vendor->name }}
+                            </a>
+                        </div>
+                        @elseif($venue && auth()->user()->isMember($venue->subdomain))
+                        <div class="mb-6">
+                            <x-input-label for="role_name" :value="__('messages.name') . ' *'" />
+                            <x-text-input id="role_name" name="role_name" type="text" class="mt-1 block w-full"
+                                :value="old('role_name')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('role_name')" />
+                        </div>
+
+                        <div class="mb-6">
+                            <x-input-label for="role_email" :value="__('messages.email') . ' *'" />
+                            <x-text-input id="role_email" name="role_email" type="email" class="mt-1 block w-full"
+                                :value="old('role_email')" required />
+                            <x-input-error class="mt-2" :messages="$errors->get('role_email')" />
+                        </div>
+                        @else
+                        <div class="mb-6">
+                            <select name="role_id"
+                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">>
+                                @foreach ($roles as $role)
+                                <option value="{{ App\Utils\UrlUtils::encodeId($role->id) }}">{{ $role->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @endif
+
+                    </div>
+                </div>
+
+                @if (! $venue || (! $venue->user_id || auth()->user()->isMember($venue->subdomain)))
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
+                    <div class="max-w-xl">
+
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
+                            {{ __('messages.details') }}
+                        </h2>
+
+                        <div class="mb-6">
+                            <x-input-label for="starts_at" :value="__('messages.date_and_time') . ($venue && $venue->user_id ? '' : ' *')" />
+                            <x-text-input type="text" id="starts_at" name="starts_at" class="datepicker"
+                                :value="old('starts_at', $event->localStartsAt())" :required="$venue && $venue->user_id ? '' : 'required'"/>
+                            <x-input-error class="mt-2" :messages="$errors->get('starts_at')" />
+                        </div>
+
+                        <div class="mb-6">
+                            <x-input-label for="duration" :value="__('messages.duration_in_hours')" />
+                            <x-text-input type="number" id="duration" name="duration"
+                                :value="old('duration', $event->duration)" />
+                            <x-input-error class="mt-2" :messages="$errors->get('duration')" />
+                        </div>
+
+                        <!--
+                        <div class="mb-6">
+                            <x-input-label for="visibility" :value="__('messages.visibility')" />
+                            <select id="visibility" name="visibility"
+                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+                                @foreach(['private', 'unlisted', 'public'] as $level)
+                                <option value="{{ $level }}" {{ $event->visibility == $level ? 'SELECTED' : '' }}>
+                                    {{ __('messages.' . $level) }}</option>
+                                @endforeach
+                            </select>
+                            <x-input-error class="mt-2" :messages="$errors->get('visibility')" />
+                        </div>
+                        -->
+
+                    </div>
+                </div>
+                @endif
+
                 @if (! $venue || ($venue && ! $venue->user_id))
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg" id="address">
                     <div class="max-w-xl">
@@ -121,120 +244,6 @@
                             <x-input-error class="mt-2" :messages="$errors->get('country')" />
                             <input type="hidden" id="venue_country_code" name="venue_country_code" />
                         </div>
-
-                    </div>
-                </div>
-                @endif
-
-                @if ($talent)
-                <input type="hidden" name="role_id" value="{{ App\Utils\UrlUtils::encodeId($talent->id) }}" />
-                @elseif ($vendor)
-                <input type="hidden" name="role_id" value="{{ App\Utils\UrlUtils::encodeId($vendor->id) }}" />
-                @else
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
-                    <div class="max-w-xl">
-
-                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
-                            @if($venue && ! $venue->accept_talent_requests)
-                            {{ __('messages.vendor') }}
-                            @elseif($venue && ! $venue->accept_vendor_requests)
-                            {{ __('messages.talent') }}
-                            @else
-                            {{ __('messages.talent_vendor') }}
-                            @endif
-                        </h2>
-
-
-                        @if($venue && auth()->user()->isMember($venue->subdomain))
-                        @if($venue->accept_talent_requests && $venue->accept_vendor_requests)
-                        <fieldset>
-                            <x-input-label for="role_type" :value="__('messages.type')" />
-                            <div class="mt-2 mb-6 space-y-6 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-                                <div class="flex items-center">
-                                    <input id="talent" name="role_type" type="radio" value="talent" CHECKED
-                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                                    <label for="talent"
-                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ __('messages.talent') }}</label>
-                                </div>
-                                <div class="flex items-center">
-                                    <input id="vendor" name="role_type" type="radio" value="vendor"
-                                        class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                                    <label for="vendor"
-                                        class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ __('messages.vendor') }}</label>
-                                </div>
-                            </div>
-                        </fieldset>
-                        @elseif($venue->accept_talent_requests)
-                        <input type="hidden" name="role_type" value="talent" />
-                        @elseif($venue->accept_vendor_requests)
-                        <input type="hidden" name="role_type" value="vendor" />
-                        @endif
-                        @endif
-
-                        @if($venue && auth()->user()->isMember($venue->subdomain))
-                        <div class="mb-6">
-                            <x-input-label for="role_name" :value="__('messages.name') . ' *'" />
-                            <x-text-input id="role_name" name="role_name" type="text" class="mt-1 block w-full"
-                                :value="old('role_name')" required autofocus />
-                            <x-input-error class="mt-2" :messages="$errors->get('role_name')" />
-                        </div>
-
-                        <div class="mb-6">
-                            <x-input-label for="role_email" :value="__('messages.email') . ' *'" />
-                            <x-text-input id="role_email" name="role_email" type="email" class="mt-1 block w-full"
-                                :value="old('role_email')" required />
-                            <x-input-error class="mt-2" :messages="$errors->get('role_email')" />
-                        </div>
-                        @else
-                        <div class="mb-6">
-                            <select name="role_id"
-                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">>
-                                @foreach ($roles as $role)
-                                <option value="{{ App\Utils\UrlUtils::encodeId($role->id) }}">{{ $role->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        @endif
-
-                    </div>
-                </div>
-                @endif
-
-                @if (! $venue || (! $venue->user_id || auth()->user()->isMember($venue->subdomain)))
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
-                    <div class="max-w-xl">
-
-                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">
-                            {{ __('messages.details') }}
-                        </h2>
-
-                        <div class="mb-6">
-                            <x-input-label for="starts_at" :value="__('messages.date_and_time') . ($venue && $venue->user_id ? '' : ' *')" />
-                            <x-text-input type="text" id="starts_at" name="starts_at" class="datepicker"
-                                :value="old('starts_at', $event->localStartsAt())" :required="$venue && $venue->user_id ? 'false' : 'true'" />
-                            <x-input-error class="mt-2" :messages="$errors->get('starts_at')" />
-                        </div>
-
-                        <div class="mb-6">
-                            <x-input-label for="duration" :value="__('messages.duration_in_hours')" />
-                            <x-text-input type="number" id="duration" name="duration"
-                                :value="old('duration', $event->duration)" />
-                            <x-input-error class="mt-2" :messages="$errors->get('duration')" />
-                        </div>
-
-                        <!--
-                        <div class="mb-6">
-                            <x-input-label for="visibility" :value="__('messages.visibility')" />
-                            <select id="visibility" name="visibility"
-                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                                @foreach(['private', 'unlisted', 'public'] as $level)
-                                <option value="{{ $level }}" {{ $event->visibility == $level ? 'SELECTED' : '' }}>
-                                    {{ __('messages.' . $level) }}</option>
-                                @endforeach
-                            </select>
-                            <x-input-error class="mt-2" :messages="$errors->get('visibility')" />
-                        </div>
-                        -->
 
                     </div>
                 </div>
