c090f8edc3493900c33da41af8fb2073ff414cbb
Hillel Coren <hillelcoren@gmail.com>
2024-08-04 13:26:07 +0300

    Events

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -15,13 +15,15 @@ class VerifyEmailController extends Controller
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('home', absolute: false).'?verified=1');
+            return redirect(route('home'))
+                ->with('message', __('messages.confirmed_email'));
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
-
-        return redirect()->intended(route('home', absolute: false).'?verified=1');
+        
+        return redirect(route('home'))
+                ->with('message', __('messages.confirmed_email'));
     }
 }
