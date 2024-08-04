6f78ccad41c243c5bbcb68b8c89532b6069cacf0
Hillel Coren <hillelcoren@gmail.com>
2024-08-04 13:22:02 +0300

    Events

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureEmailIsVerified
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user() && ! $request->user()->hasVerifiedEmail() && ! $this->isVerificationRoute($request)) {        
+            return redirect()->route('verification.notice');
+        }
+
+        return $next($request);
+    }
+
+    protected function isVerificationRoute($request)
+    {
+        return $request->routeIs('verification.notice')
+            || $request->routeIs('verification.verify')
+            || $request->routeIs('verification.send')
+            || $request->routeIs('landing');
+    }
+}
