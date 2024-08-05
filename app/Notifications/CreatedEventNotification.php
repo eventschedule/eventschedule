78a96f63bed3c2f20334e845eb42741279c1fadd
Hillel Coren <hillelcoren@gmail.com>
2024-08-05 12:03:41 +0300

    Events

---
+++ app/Notifications/CreatedEventNotification.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CreatedEventNotification extends Notification
+{
+    use Queueable;
+
+    protected $event;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $role = $this->event->role;
+        $venue = $this->event->venue;
+        $user = $this->event->user;
+
+        return (new MailMessage)
+                    ->subject(str_replace(
+                        [':name'], 
+                        [$role->name],
+                        __('messages.event_has_been_created')))
+                    ->line(str_replace(
+                        [':user', ':role', ':name'], 
+                        [$user->name, $role->name, $venue->name], 
+                        __('messages.event_has_been_created_details')))
+                    ->action(__('messages.sign_up'), url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
