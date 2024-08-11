c13072be8c55e9cd2bf3ae0de6bcdab5b92bc1ef
Hillel Coren <hillelcoren@gmail.com>
2024-08-11 14:24:13 +0300

    Events

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,36 @@
     }
     gtag('js', new Date());
     gtag('config', '{{ config('services.google.analytics') }}');
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const sidebar = document.getElementById('sidebar');
+        const openButton = document.getElementById('open-sidebar');
+        const closeButton = document.getElementById('close-sidebar');
+
+        function toggleMenu() {
+            const isOpen = sidebar.getAttribute('data-state') === 'open';
+            const backdrop = sidebar.getElementsByClassName('bg-gray-900')[0]; // Selects the backdrop
+            const menu = sidebar.querySelector('.fixed.flex > div'); // Selects the menu
+
+            if (isOpen) {
+                sidebar.setAttribute('data-state', 'closed');
+                backdrop.classList.add('opacity-0');
+                backdrop.classList.remove('opacity-100');
+                menu.classList.add('-translate-x-full');
+                menu.classList.remove('translate-x-0');
+            } else {
+                sidebar.setAttribute('data-state', 'open');
+                backdrop.classList.add('opacity-100');
+                backdrop.classList.remove('opacity-0');
+                menu.classList.add('translate-x-0');
+                menu.classList.remove('-translate-x-full');
+            }
+        }
+
+        openButton.addEventListener('click', toggleMenu);
+        closeButton.addEventListener('click', toggleMenu);
+    });
+
     </script>
 
     <meta charset="utf-8">
@@ -55,44 +85,14 @@
 
     <div>
         <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-        <div class="relative z-50 lg:hidden" role="dialog" aria-modal="true">
-            <!--
-      Off-canvas menu backdrop, show/hide based on off-canvas menu state.
-
-      Entering: "transition-opacity ease-linear duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "transition-opacity ease-linear duration-300"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
+        <div data-state="closed" id="sidebar" class="relative z-50 hidden" role="dialog" aria-modal="true">
             <div class="fixed inset-0 bg-gray-900/80" aria-hidden="true"></div>
 
             <div class="fixed inset-0 flex">
-                <!--
-        Off-canvas menu, show/hide based on off-canvas menu state.
-
-        Entering: "transition ease-in-out duration-300 transform"
-          From: "-translate-x-full"
-          To: "translate-x-0"
-        Leaving: "transition ease-in-out duration-300 transform"
-          From: "translate-x-0"
-          To: "-translate-x-full"
-      -->
                 <div class="relative mr-16 flex w-full max-w-xs flex-1">
-                    <!--
-          Close button, show/hide based on off-canvas menu state.
-
-          Entering: "ease-in-out duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-          Leaving: "ease-in-out duration-300"
-            From: "opacity-100"
-            To: "opacity-0"
-        -->
                     <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                        <button type="button" class="-m-2.5 p-2.5">
-                            <span class="sr-only">Close sidebar</span>
+                        <button id="close-sidebar" type="button" class="-m-2.5 p-2.5">
+                            <span class="sr-only">{{ __('messages.close_sidebar') }}</span>
                             <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -112,7 +112,7 @@
                             </div>
                         </a>
                         <nav class="flex flex-1 flex-col">
-                            <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                            <ul role="list" class="flex flex-1 flex-col Xgap-y-7">
                                 <li>
                                     <ul role="list" class="-mx-2 space-y-1">
                                         <li>
@@ -124,64 +124,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                         d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                                                 </svg>
-                                                Dashboard 1
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                                                </svg>
-                                                Team
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
-                                                </svg>
-                                                Projects
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
-                                                </svg>
-                                                Calendar
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
-                                                </svg>
-                                                Documents
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-                                                </svg>
-                                                Reports
+                                                Dashboard
                                             </a>
                                         </li>
                                     </ul>
@@ -190,28 +133,12 @@
                                     <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
                                     <ul role="list" class="-mx-2 mt-2 space-y-1">
                                         <li>
-                                            Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800"
+                                            <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
                                             <a href="#"
                                                 class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
                                                 <span
                                                     class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">H</span>
                                                 <span class="truncate">Heroicons</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <span
-                                                    class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">T</span>
-                                                <span class="truncate">Tailwind Labs</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <span
-                                                    class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">W</span>
-                                                <span class="truncate">Workcation</span>
                                             </a>
                                         </li>
                                     </ul>
@@ -241,8 +168,8 @@
         <div class="lg:pl-72">
             <div
                 class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-                <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
-                    <span class="sr-only">Open sidebar</span>
+                <button id="open-sidebar" type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+                    <span class="sr-only">{{ __('messages.open_sidebar') }}</span>
                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                         aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -256,14 +183,14 @@
                 <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
                     <div class="relative flex flex-1"></div>
                     <!--
-        <form class="relative flex flex-1" action="#" method="GET">
-          <label for="search-field" class="sr-only">Search</label>
-          <svg class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-          </svg>
-          <input id="search-field" class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." type="search" name="search">
-        </form>
-        -->
+                    <form class="relative flex flex-1" action="#" method="GET">
+                    <label for="search-field" class="sr-only">Search</label>
+                    <svg class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                    </svg>
+                    <input id="search-field" class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." type="search" name="search">
+                    </form>
+                    -->
                     <div class="flex items-center gap-x-4 lg:gap-x-6">
                         <!--
                         <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
