49b9d78e343e5cb0602626c9a9c0ae904465a1d8
Hillel Coren <hillelcoren@gmail.com>
2024-08-11 14:27:42 +0300

    Events

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -30,26 +30,7 @@
                     </svg>
                 </a>
             </div>
-            <div class="md:flex md:items-center">
-                <!--
-                <div class="relative">
-                <button type="button" class="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="false" aria-haspopup="true">
-                    Month view
-                    <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                    </svg>
-                </button>
-                <div class="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                    <div class="py-1" role="none">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Day view</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Week view</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">Month view</a>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Year view</a>
-                    </div>
-                </div>
-                </div>
-                -->
-                @if ($showAdd)
+            @if ($showAdd)
                 <div class="ml-3 h-6 w-px bg-gray-300"></div>
                 <a href="{{ route('event.create', ['subdomain' => $role->subdomain]) }}">
                     <button type="button"
@@ -61,42 +42,8 @@
                         {{ __('messages.add_event') }}
                     </button>
                 </a>
-                @endif
-                <!--
-            <div class="relative ml-6 md:hidden">
-                <button type="button"
-                    class="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500"
-                    id="menu-0-button" aria-expanded="false" aria-haspopup="true">
-                    <span class="sr-only">Open menu</span>
-                    <svg class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                        <path
-                            d="M3 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM8.5 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM15.5 8.5a.75.75 0 100 3 .75.75 0 000-3z" />
-                    </svg>
-                </button>
-                <div class="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu" aria-orientation="vertical" aria-labelledby="menu-0-button" tabindex="-1">
-                    <div class="py-1" role="none">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-0">Add Event</a>
-                    </div>
-                    <div class="py-1" role="none">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-1">Go to today</a>
-                    </div>
-                    <div class="py-1" role="none">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-2">Day view</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-3">Week view</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-4">Month view</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="menu-0-item-5">Year view</a>
-                    </div>
-                </div>
-            </div>
-            -->
-            </div>
+            @endif
+        </div>
     </header>
     <div class="shadow-md ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
         <div
