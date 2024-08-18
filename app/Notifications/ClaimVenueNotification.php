e59f129f2399c82defa7de2036374ce95a56b9b2
Hillel Coren <hillelcoren@gmail.com>
2024-08-18 18:04:48 +0300

    Events

--- app/Notifications/ClaimVenueNotification.php
+++ app/Notifications/ClaimVenueNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Utils\UrlUtils;
 
 class ClaimVenueNotification extends Notification
 {
@@ -43,14 +44,14 @@ class ClaimVenueNotification extends Notification
         return (new MailMessage)
                     ->replyTo($user->email, $user->name)
                     ->subject(str_replace(
-                        [':user', ':venue'], 
-                        [$user->name, $venue->name],
+                        [':role', ':venue'], 
+                        [$role->name, $venue->name],
                         __('messages.claim_your_venue')))
-                    ->line(str_replace(
-                        [':user', ':role', ':venue'], 
-                        [$user->name, $role->name, $venue->name], 
-                        __('messages.claim_your_venue_details')))
-                    ->action(__('messages.sign_up'), route('sign_up', ['email' => $venue->email]));
+                    ->line(__('messages.claim_your_venue_details'))
+                    ->action(__('messages.view_event'), route('role.view_guest', ['hash' => UrlUtils::encodeId($this->event->id)]))
+                    ->line(__('messages.claim_email_line2'))
+                    ->action(__('messages.sign_up'), route('sign_up', ['email' => $venue->email]))
+                    ->line(__('messages.claim_email_line3'));
     }
 
     /**
