3683be230664edaac4ef56dad5bae3430a35097d
Hillel Coren <hillelcoren@gmail.com>
2024-08-18 18:04:48 +0300

    Events

--- app/Notifications/ClaimRoleNotification.php
+++ app/Notifications/ClaimRoleNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Utils\UrlUtils;
 
 class ClaimRoleNotification extends Notification
 {
@@ -43,14 +44,14 @@ class ClaimRoleNotification extends Notification
         return (new MailMessage)
                     ->replyTo($user->email, $user->name)
                     ->subject(str_replace(
-                        [':user', ':role'], 
-                        [$user->name, $role->name],
+                        [':venue', ':role'], 
+                        [$venue->name, $role->name],
                         __('messages.claim_your_role')))
-                    ->line(str_replace(
-                        [':user', ':role', ':venue'], 
-                        [$user->name, $role->name, $venue->name], 
-                        __('messages.claim_your_role_details')))
-                    ->action(__('messages.sign_up'), route('sign_up', ['email' => $role->email]));
+                    ->line(__('messages.claim_your_role_details'))
+                    ->action(__('messages.view_event'), route('role.view_guest', ['hash' => UrlUtils::encodeId($this->event->id)]))
+                    ->line(__('messages.claim_email_line2'))
+                    ->action(__('messages.sign_up'), route('sign_up', ['email' => $role->email]))
+                    ->line(__('messages.claim_email_line3'));
     }
 
     /**
