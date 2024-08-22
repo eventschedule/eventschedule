7688d0575939428aeff6d55c650c16cf576f2a1b
Hillel Coren <hillelcoren@gmail.com>
2024-08-23 00:32:06 +0300

    Roles

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -213,6 +213,11 @@ class EventController extends Controller
             $venue->state = $request->venue_state;
             $venue->postal_code = $request->venue_postal_code;
             $venue->country_code = $request->venue_country_code;
+            $venue->formatted_address = $request->formatted_address;
+            $venue->google_place_id = $request->google_place_id;
+            $venue->geo_address = $request->geo_address;
+            $venue->geo_lat = $request->geo_lat;
+            $venue->geo_lon = $request->geo_lon;
             $venue->save();
         }
 
