d28c3d117ebad07a7eed49d8aa03474624b22143
Hillel Coren <hillelcoren@gmail.com>
2024-08-25 17:24:46 +0300

    Roles

---
+++ database/migrations/2024_08_25_141207_add_show_email_address.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->boolean('show_email')->default(false);
+            $table->string('plan_expires')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropColumn('show_email');
+            $table->dropColumn('plan_expires');
+        });
+    }
+};
