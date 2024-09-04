929cde73abe34b3288c1ae44edbab3fec7162ce1
Hillel Coren <hillelcoren@gmail.com>
2024-09-04 13:06:22 +0300

    Curators

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -214,7 +214,7 @@
                     </div>
                 </div>
 
-                @if (! $role->canHaveAddress())
+                @if ($role->canHaveAddress())
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                     <div class="max-w-xl">
 
