de7e29cbdff0112be049c9a8a9cbd3dd139624c2
Hillel Coren <hillelcoren@gmail.com>
2024-09-10 12:27:47 +0300

    Emails

--- resources/views/mail/role/claim.blade.php
+++ resources/views/mail/role/claim.blade.php
@@ -1,11 +1,13 @@
 <x-mail::message>
-# Introduction
+# {{ __('messages.hello') }}!
 
-The body of your message.
+{{ str_replace([':venue', ':role'], [$venue->name, $role->name], __('messages.claim_your_role'))}}
 
-<x-mail::button :url="''">
-Button Text
+<x-mail::button :url="route('event.view_guest', ['subdomain' => $venue->subdomain, 'hash' => App\Utils\UrlUtils::encodeId($event->id)])">
+{{ __('messages.view_event') }}
 </x-mail::button>
+
+{{ __('messages.claim_email_line2') }}
 
 Thanks,<br>
 {{ config('app.name') }}
