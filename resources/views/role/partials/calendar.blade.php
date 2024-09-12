aeab1636910671655dbbc6ffdf3d2223773c6aa7
Hillel Coren <hillelcoren@gmail.com>
2024-09-12 18:25:09 +0300

    Roles

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -51,11 +51,11 @@
             </button>
             @endif
             @elseif ($route == 'home')
-            <div style="font-family: sans-serif" class="mt-8 relative inline-block text-left">
+            <div style="font-family: sans-serif" class="ml-3 shadow-md relative inline-block text-left">
                 <button type="button" 
                     onclick="onPopUpClick()"
                     class="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                    {{ strtoupper(__('messages.new_schedule')) }}
+                    {{ __('messages.new_schedule') }}
                     <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                     </svg>
