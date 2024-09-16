75a0bd933574c844fca0b3970a0084e926a4285c
Hillel Coren <hillelcoren@gmail.com>
2024-09-16 14:55:39 +0300

    Refactor

--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -3,6 +3,7 @@
     <x-slot name="head">
         <link rel="preconnect" href="https://rsms.me/">
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">        
+        {{ isset($head) ? $head : '' }}
     </x-slot>
 
     <div>
