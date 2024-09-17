17394ff594732615ff252bd65333d660d9230961
Hillel Coren <hillelcoren@gmail.com>
2024-09-17 15:01:54 +0300

    Roles

---
+++ database/migrations/2024_09_17_111106_add_is_open.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->foreignId('venue_id')->nullable(false)->change();
+            $table->foreignId('role_id')->nullable(false)->change();
+        });        
+
+        Schema::table('roles', function (Blueprint $table) {
+            $table->string('subdomain')->nullable()->change();
+            $table->string('email')->nullable()->change();
+            $table->boolean('is_open')->default(false);            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->string('subdomain')->nullable(false)->change();
+            $table->string('email')->nullable(false)->change();
+            $table->dropColumn('is_open');
+        });
+
+        Schema::table('events', function (Blueprint $table) {
+            $table->foreignId('venue_id')->nullable()->change();
+            $table->foreignId('role_id')->nullable()->change();
+        });
+    }
+};
