f969fc9b1e3f9f1af588699c46c2fa270fc2ed86
Hillel Coren <hillelcoren@gmail.com>
2024-09-23 17:32:20 +0300

    Roles

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 if (config('app.env') != 'local') {
     Route::domain('{subdomain}.eventschedule.com')->group(function () {
         Route::get('/sign_up', [RoleController::class, 'signUp'])->name('event.sign_up');
+        Route::get('/unsubscribe', [RoleController::class, 'showUnsubscribe'])->name('role.show_unsubscribe');
         Route::post('/unsubscribe', [RoleController::class, 'unsubscribe'])->name('role.unsubscribe')->middleware('throttle:2,2');
         Route::get('/follow', [RoleController::class, 'follow'])->name('role.follow');
         Route::get('/{other_subdomain?}', [RoleController::class, 'viewGuest'])->name('event.view_guest');
@@ -72,7 +73,8 @@ if (config('app.env') == 'local') {
         Route::get('/{subdomain}/view/{other_subdomain?}', [RoleController::class, 'viewGuest'])->name('event.view_guest');
         Route::get('/{subdomain}/view', [RoleController::class, 'viewGuest'])->name('role.view_guest');
         Route::get('/{subdomain}/sign_up', [RoleController::class, 'signUp'])->name('event.sign_up');
-        Route::post('/{subdomain}/unsubscribe', [RoleController::class, 'unsubscribe'])->name('role.unsubscribe')->middleware('throttle:2,2');
+        Route::get('/unsubscribe', [RoleController::class, 'showUnsubscribe'])->name('role.show_unsubscribe');
+        Route::post('/unsubscribe', [RoleController::class, 'unsubscribe'])->name('role.unsubscribe')->middleware('throttle:2,2');
         Route::get('/{subdomain}/follow', [RoleController::class, 'follow'])->name('role.follow');
     });    
 } else {
