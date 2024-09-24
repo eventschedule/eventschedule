02a057da8624d8b06fc729df995f2067acb4d96b
Hillel Coren <hillelcoren@gmail.com>
2024-09-24 22:55:33 +0300

    Events

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -4,16 +4,41 @@
   <script src="https://unpkg.com/vue@3/dist/vue.global.prod.js"></script>
 </x-slot>
 
-<div id="app">@{{ message }}</div>
+<div id="app">
+  <h2 class="pt-2 mt-4 text-xl font-bold leading-7 text-gray-900 dark:text-gray-100 sm:truncate sm:text-2xl sm:tracking-tight">
+    {{ $title }}
+  </h2>
+
+  <form method="POST"
+        action="{{ $event->exists ? route('event.update', ['subdomain' => $subdomain, 'hash' => App\Utils\UrlUtils::encodeId($event->id)]) : route('event.store', ['subdomain' => $subdomain]) }}"
+        enctype="multipart/form-data">
+
+        @csrf
+
+        @if ($event->exists)
+        @method('put')
+        @endif
+
+        <div class="py-5">
+            <div class="max-w-7xl mx-auto space-y-6">
+                @{{ event }}
+            </div>
+        </div>
+    </form>
+</div>
 
 <script>
   const { createApp, ref } = Vue
 
   createApp({
-    setup() {
-      const message = ref('Hello vue!')
+    data() {
       return {
-        message
+        event: @json($event),
+      }
+    },
+    setup() {
+      return {
+        
       }
     }
   }).mount('#app')
