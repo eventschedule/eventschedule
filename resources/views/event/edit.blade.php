8b1474a049405f004a95d5e6f776328a65c36688
Hillel Coren <hillelcoren@gmail.com>
2024-09-26 21:12:37 +0300

    Events

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -357,7 +357,7 @@
 
                             <div v-if="memberType === 'use_existing'">
                                 <select v-model="selectedExistingMember" @change="addExistingMember" 
-                                    class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full pl-3 pr-10 py-2 text-base">
                                     <option value="" disabled selected>{{ __('messages.please_select') }}</option>
                                     <option v-for="member in members" :key="member.id" :value="member">@{{ member.name }}</option>
                                 </select>
@@ -579,7 +579,7 @@
         selectedMembers: @json($event->members ?? []),
         memberSearchEmail: "",
         memberSearchResults: [],
-        selectedExistingMember: null,
+        selectedExistingMember: "",
         memberEmail: "",
         memberName: "",
       }
