834e4c1ec40b67aceca364e3a9327cdf6b9ce669
Hillel Coren <hillelcoren@gmail.com>
2024-10-08 22:22:23 +0300

    Events

--- resources/views/role/show-admin.blade.php
+++ resources/views/role/show-admin.blade.php
@@ -190,7 +190,7 @@
                                 <path
                                     d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
                             </svg>
-                            {{ __('messages.edit') }}
+                            {{ __('messages.edit_' . strtolower($role->type)) }}
                         </button>
                     </a>
                 </span>
@@ -206,7 +206,7 @@
                                 <path
                                     d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
                             </svg>
-                            {{ __('messages.view') }}
+                            {{ __('messages.view_' . strtolower($role->type)) }}
                         </button>
                     </a>
                 </span>
