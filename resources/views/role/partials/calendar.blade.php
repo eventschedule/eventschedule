d50bdd90a4d5cc304582dab5e74a0f8f030373d0
Hillel Coren <hillelcoren@gmail.com>
2024-10-28 12:24:17 +0200

    Events

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -54,7 +54,7 @@
             <div style="font-family: sans-serif" class="ml-3 shadow-sm relative inline-block text-left">
                 <button type="button" 
                     onclick="onPopUpClick('calendar-pop-up-menu', event)"
-                    class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                    class="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                     {{ __('messages.new_schedule') }}
                     <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
