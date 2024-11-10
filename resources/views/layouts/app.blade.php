67183992616f0b6e89ec3bc20471f478eaaa66aa
Hillel Coren <hillelcoren@gmail.com>
2024-11-10 18:11:10 +0200

    Sentry

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,10 @@
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.99em%22 font-size=%2275%22>📅</text></svg>">
     <link rel="canonical" href="{{ url()->current() }}">
 
+    @if (config('services.sentry.dsn'))
+        <script src="https://js.sentry-cdn.com/dbdc1097b55788aa2f56e31faa53a7fc.min.js" crossorigin="anonymous"></script>
+    @endif
+
     @if (isset($meta))
         {{ $meta }}
     @else
