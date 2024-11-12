e7f0ae85ceb33226c98430f88481b9291a06ff05
Hillel Coren <hillelcoren@gmail.com>
2024-11-12 12:34:58 +0200

    Roles

--- resources/views/role/show-guest.blade.php
+++ resources/views/role/show-guest.blade.php
@@ -185,7 +185,7 @@
 
           @if($role->description_html)
           <div
-            class="bg-[#F5F9FE] rounded-2xl px-6 lg:px-16 py-12 flex flex-col gap-6"
+            class="bg-[#F5F9FE] rounded-2xl px-6 lg:px-16 py-12 flex flex-col gap-6 mb-6"
           >
             <h3 class="text-[32px] font-semibold leading-10 text-[#151B26]">
               {{ __('messages.about') }}
@@ -197,12 +197,10 @@
           </div>
           @endif
 
+        <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6">          
+
         </div>
-      </div>
-      <div class="container mx-auto px-5 py-[80px]">
-        <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6">
-          
-        </div>
+
         <div class="bg-[#F5F9FE] rounded-2xl p-8">
           <h3 class="text-[32px] font-semibold leading-10 text-[#151B26] mb-6">
             {{ __('messages.social_media') }}
