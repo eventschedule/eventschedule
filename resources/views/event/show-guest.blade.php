f093f314298ee2446aa437252b32f1a4fc155808
Hillel Coren <hillelcoren@gmail.com>
2024-11-14 18:09:24 +0200

    Roles

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -251,7 +251,7 @@
           <div class="flex flex-col gap-4">
             @if ($event->venue->phone)
             <div
-              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white hover:text-[#151B26] hover:fill-[#151B26]"
+              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white"
             >
               <svg
                 width="24"
@@ -263,14 +263,14 @@
                   d="M10.0376 5.31617L10.6866 6.4791C11.2723 7.52858 11.0372 8.90532 10.1147 9.8278C10.1147 9.8278 10.1147 9.8278 10.1147 9.8278C10.1146 9.82792 8.99588 10.9468 11.0245 12.9755C13.0525 15.0035 14.1714 13.8861 14.1722 13.8853C14.1722 13.8853 14.1722 13.8853 14.1722 13.8853C15.0947 12.9628 16.4714 12.7277 17.5209 13.3134L18.6838 13.9624C20.2686 14.8468 20.4557 17.0692 19.0628 18.4622C18.2258 19.2992 17.2004 19.9505 16.0669 19.9934C14.1588 20.0658 10.9183 19.5829 7.6677 16.3323C4.41713 13.0817 3.93421 9.84122 4.00655 7.93309C4.04952 6.7996 4.7008 5.77423 5.53781 4.93723C6.93076 3.54428 9.15317 3.73144 10.0376 5.31617Z"
                 />
               </svg>
-              <a href="tel:{{ $event->venue->phone }}" class="text-sm"
+              <a href="tel:{{ $event->venue->phone }}" class="text-sm hover:underline"
                 >{{ $event->venue->phone }}</a
               >
             </div>
             @endif
             @if ($event->venue->email)
             <div
-              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white hover:text-[#151B26] hover:fill-[#151B26]"
+              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white"
             >
               <svg
                 width="24"
@@ -284,14 +284,14 @@
                   d="M3.17157 5.17157C2 6.34315 2 8.22876 2 12C2 15.7712 2 17.6569 3.17157 18.8284C4.34315 20 6.22876 20 10 20H14C17.7712 20 19.6569 20 20.8284 18.8284C22 17.6569 22 15.7712 22 12C22 8.22876 22 6.34315 20.8284 5.17157C19.6569 4 17.7712 4 14 4H10C6.22876 4 4.34315 4 3.17157 5.17157ZM18.5762 7.51986C18.8413 7.83807 18.7983 8.31099 18.4801 8.57617L16.2837 10.4066C15.3973 11.1452 14.6789 11.7439 14.0448 12.1517C13.3843 12.5765 12.7411 12.8449 12 12.8449C11.2589 12.8449 10.6157 12.5765 9.95518 12.1517C9.32112 11.7439 8.60271 11.1452 7.71636 10.4066L5.51986 8.57617C5.20165 8.31099 5.15866 7.83807 5.42383 7.51986C5.68901 7.20165 6.16193 7.15866 6.48014 7.42383L8.63903 9.22291C9.57199 10.0004 10.2197 10.5384 10.7666 10.8901C11.2959 11.2306 11.6549 11.3449 12 11.3449C12.3451 11.3449 12.7041 11.2306 13.2334 10.8901C13.7803 10.5384 14.428 10.0004 15.361 9.22291L17.5199 7.42383C17.8381 7.15866 18.311 7.20165 18.5762 7.51986Z"
                 />
               </svg>
-              <a href="mailto:{{ $role->email }}" class="text-sm"
+              <a href="mailto:{{ $role->email }}" class="text-sm hover:underline"
                 >{{ $event->venue->email }}</a
               >
             </div>
             @endif
             @if ($event->venue->website)
             <div
-              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white hover:text-[#151B26] hover:fill-[#151B26]"
+              class="flex flex-row gap-2 items-center relative duration-300 text-white fill-white"
             >
               <svg
                 width="24"
@@ -317,7 +317,9 @@
                   d="M16.669 15.6739C16.4501 16.8413 16.1267 17.9171 15.7104 18.8368C15.2966 19.7512 14.7849 20.5247 14.1793 21.0789C13.5708 21.6358 12.831 22 12.0001 22C17.2707 22 21.5889 17.9226 21.9724 12.75H16.9868C16.9512 13.7515 16.8445 14.7381 16.669 15.6739Z"
                 />
               </svg>
-              <a href="{{ $event->venue->website }}" class="text-sm">{{ $event->venue->website }}</a>
+              <a href="{{ $event->venue->website }}" class="text-sm hover:underline">
+                {{ $event->venue->website }}
+              </a>
             </div>
             @endif
           </div>
