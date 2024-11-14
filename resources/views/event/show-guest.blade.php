84ccb80bda31d31b5a7fc64b8fdd7ae60d8f3428
Hillel Coren <hillelcoren@gmail.com>
2024-11-14 16:11:02 +0200

    Roles

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -34,10 +34,16 @@
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z"
+                d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z"
               />
             </svg>
-            <p class="text-sm">{{ $event->venue->name }}</p>
+            @if ($event->venue->isClaimed())
+              <a href="{{ route('event.view_guest', ['subdomain' => $event->venue->subdomain]) }}" class="text-sm hover:underline">
+                {{ $event->venue->name }}
+              </a>
+            @else
+              <p class="text-sm">{{ $event->venue->name }}</p>
+            @endif
           </div>
           <div
             class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white"
@@ -93,7 +99,11 @@
                 d="M12 2C7.58172 2 4 6.00258 4 10.5C4 14.9622 6.55332 19.8124 10.5371 21.6744C11.4657 22.1085 12.5343 22.1085 13.4629 21.6744C17.4467 19.8124 20 14.9622 20 10.5C20 6.00258 16.4183 2 12 2ZM12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z"
               />
             </svg>
-            <p class="text-sm">123 Serenity Lane, Wellness City</p>
+            <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode($event->venue->bestAddress()) }}" target="_blank">
+              <p class="text-sm hover:underline">
+                {{ $event->venue->shortAddress() }}
+              </p>
+            </a>
           </div>
         </div>
       </div>
