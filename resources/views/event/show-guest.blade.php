6d17ddfd44fcf8fc7110a24b9a3a6313e33393eb
Hillel Coren <hillelcoren@gmail.com>
2024-11-15 00:39:12 +0200

    Roles

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -31,10 +31,9 @@
           <div style="font-family: sans-serif" class="mt-8 relative inline-block text-left">
               <button type="button" 
                   onclick="onPopUpClick('calendar-pop-up-menu', event)"
-                  style="background-color: {{ $role->accent_color ?? '#4E81FA' }};"
-                  class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-4 py-2 text-lg font-semibold text-white shadow-sm hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                  class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-6 py-3 text-lg font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
               {{ __('messages.add_to_calendar') }}
-              <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="white" aria-hidden="true">
+              <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
               </svg>
               </button>
