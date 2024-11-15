6eac6ea26472772c127ab45553662b67432c47ae
Hillel Coren <hillelcoren@gmail.com>
2024-11-15 15:37:18 +0200

    Roles

--- resources/views/role/show-admin.blade.php
+++ resources/views/role/show-admin.blade.php
@@ -181,18 +181,6 @@
 
             <div class="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
 
-                <span class="hidden sm:block">
-                    <a href="{{ route('role.view_guest', ['subdomain' => $role->subdomain, 'embed' => 'true']) }}" target="_blank">
-                        <button type="button"
-                            class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-3">
-                            <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
-                                <path
-                                    d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
-                            </svg>
-                            {{ __('messages.embed') }}
-                        </button>
-                    </a>
-                </span>
 
                 <span class="hidden sm:block">
                     <a href="{{ route('role.edit', ['subdomain' => $role->subdomain]) }}">
