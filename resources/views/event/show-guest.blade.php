85bb41cb1afc9a324876f047dd82302218d9188f
Hillel Coren <hillelcoren@gmail.com>
2024-11-15 15:23:57 +0200

    Roles

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -78,12 +78,16 @@
                 d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z"
               />
             </svg>
-            @if ($event->venue->isClaimed())
-              <a href="{{ route('event.view_guest', ['subdomain' => $event->venue->subdomain]) }}" class="text-sm hover:underline">
-                {{ $event->venue->name }}
-              </a>
+            @if ($event->venue)
+              @if ($event->venue->isClaimed())
+                <a href="{{ route('event.view_guest', ['subdomain' => $event->venue->subdomain]) }}" class="text-sm hover:underline">
+                  {{ $event->venue->name }}
+                </a>
+              @else
+                <p class="text-sm">{{ $event->venue->name }}</p>
+              @endif
             @else
-              <p class="text-sm">{{ $event->venue->name }}</p>
+              <p class="text-sm">{{ $event->getEventUrlDomain() }}</p>
             @endif
           </div>
           <div
@@ -107,7 +111,7 @@
             <p class="text-sm">{{ $event->getStartDateTime($date)->format('F j, Y') }}</p>
           </div>
           <div
-            class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white"
+            class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 {{ $event->venue ? "sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white" : "" }}"
           >
             <svg
               width="24"
@@ -125,6 +129,7 @@
               {{ $event->getStartEndTime($date) }}
             </p>
           </div>
+          @if ($event->venue)
           <div
             class="flex flex-row gap-2 items-center relative text-white fill-white"
           >
@@ -146,6 +151,7 @@
               </p>
             </a>
           </div>
+          @endif
         </div>
       </div>
     </div>
