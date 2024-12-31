6614e95b8dfdc3715acfd0473a0ff1e8dbecaccb
Hillel Coren <hillelcoren@gmail.com>
2024-12-31 16:29:02 +0200

    Roles

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -34,6 +34,11 @@ class Event extends Model
         });
     }
 
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
