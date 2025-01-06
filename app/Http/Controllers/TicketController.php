42730eb6a25be0f354b500a5de3dd706fbb0e9f2
Hillel Coren <hillelcoren@gmail.com>
2025-01-06 09:24:43 +0200

    Stripe

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Sale;
 use Illuminate\Support\Str;
+use App\Models\Sale;
 use App\Utils\UrlUtils;
 use Stripe\StripeClient;
 
