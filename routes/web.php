33201f52fe11ad83acf9f827b0ed568de70e2213
Hillel Coren <hillelcoren@gmail.com>
2025-01-08 16:58:20 +0200

    Stripe

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,10 @@ Route::get('/terms', [HomeController::class, 'terms'])->name('terms');
 Route::post('/message', [HomeController::class, 'message'])->name('message')->middleware('throttle:2,2');
 Route::get('/unsubscribe', [RoleController::class, 'showUnsubscribe'])->name('role.show_unsubscribe');
 Route::post('/unsubscribe', [RoleController::class, 'unsubscribe'])->name('role.unsubscribe')->middleware('throttle:2,2');
+
 Route::post('/stripe/webhook', [StripeController::class, 'webhook'])->name('stripe.webhook');
+Route::get('/ticket/qr_code/{event_id}/{secret}', [TicketController::class, 'qrCode'])->name('ticket.qr_code');
+Route::get('/ticket/view/{event_id}/{secret}', [TicketController::class, 'view'])->name('ticket.view');
 
 Route::middleware(['auth', 'verified'])->group(function () 
 {
