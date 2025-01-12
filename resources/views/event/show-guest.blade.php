4002f3c7c6a3c115e2defa95467f1098a6d4f401
Hillel Coren <hillelcoren@gmail.com>
2025-01-12 21:06:24 +0200

    Tickets

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -467,22 +467,31 @@
         </div>
         @endif
 
-        <!--
+        @if ($event->tickets_enabled && $event->isPro())
         <div class="p-6 rounded-xl flex flex-col gap-6 bg-[#F5F9FE]">
           <h4 class="text-[#151B26] text-[24px] leading-snug font-semibold">
-            Add to Calendar
+            {{ __('messages.add_to_calendar') }}
           </h4>
-          <div class="flex flex-row gap-3">
-            <a href="#" target="_blank"
-              ><img src="./images/google-meet.svg" class="w-[44px] h-[44px]"
-            /></a>
-            <a href="#" target="_blank"
-              ><img src="./images/teams.svg" class="w-[44px] h-[44px]"
-            /></a>
+          <div class="flex flex-row gap-3">          
+          <a href="{{ $event->getGoogleCalendarUrl($date) }}" target="_blank" title="Google">
+              <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path d="M21.35,11.1H12.18V13.83H18.69C18.36,17.64 15.19,19.27 12.19,19.27C8.36,19.27 5,16.25 5,12C5,7.9 8.2,4.73 12.2,4.73C15.29,4.73 17.1,6.7 17.1,6.7L19,4.72C19,4.72 16.56,2 12.1,2C6.42,2 2.03,6.8 2.03,12C2.03,17.05 6.16,22 12.25,22C17.6,22 21.5,18.33 21.5,12.91C21.5,11.76 21.35,11.1 21.35,11.1V11.1Z" />
+              </svg>
+            </a>
+            <a href="{{ $event->getAppleCalendarUrl($date) }}" target="_blank" title="Apple">
+              <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path d="M18.71,19.5C17.88,20.74 17,21.95 15.66,21.97C14.32,22 13.89,21.18 12.37,21.18C10.84,21.18 10.37,21.95 9.1,22C7.79,22.05 6.8,20.68 5.96,19.47C4.25,17 2.94,12.45 4.7,9.39C5.57,7.87 7.13,6.91 8.82,6.88C10.1,6.86 11.32,7.75 12.11,7.75C12.89,7.75 14.37,6.68 15.92,6.84C16.57,6.87 18.39,7.1 19.56,8.82C19.47,8.88 17.39,10.1 17.41,12.63C17.44,15.65 20.06,16.66 20.09,16.67C20.06,16.74 19.67,18.11 18.71,19.5M13,3.5C13.73,2.67 14.94,2.04 15.94,2C16.07,3.17 15.6,4.35 14.9,5.19C14.21,6.04 13.07,6.7 11.95,6.61C11.8,5.46 12.36,4.26 13,3.5Z" />
+              </svg>
+            </a>
+            <a href="{{ $event->getMicrosoftCalendarUrl($date) }}" target="_blank" title="Microsoft">
+              <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path d="M2,3H11V12H2V3M11,22H2V13H11V22M21,3V12H12V3H21M21,22H12V13H21V22Z" />
+              </svg>
+            </a>
           </div>
         </div>
-        -->
-        
+        @endif
+
         <div class="p-6 rounded-xl flex flex-col gap-6 bg-[#F5F9FE]">
           <h4 class="text-[#151B26] text-[24px] leading-snug font-semibold">
             {{ __('messages.create_your_own_event_schedule') }}
