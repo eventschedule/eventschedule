e74caca29f0391f5edb28163dfbff752b0690a16
Hillel Coren <hillelcoren@gmail.com>
2025-01-13 13:13:53 +0200

    Tickets

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -339,4 +339,5 @@ return [
     'payment_must_be_completed_within_hours' => 'Payment must be completed within :count hours',
     'expire_unpaid_tickets' => 'Expire unpaid tickets',
     'after_number_of_hours' => 'After number of hours',
+    'create_account' => 'Create account',
 ];
