0e4fc4ce2cea107e4beb1c4e19f349ce60269708
Hillel Coren <hillelcoren@gmail.com>
2025-01-17 11:27:47 +0200

    Tickets

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -34,6 +34,7 @@ class TicketController extends Controller
         $user = auth()->user();
 
         $sales = Sale::with('event', 'saleTickets')
+            ->whereIn('status', ['paid', 'pending']) // TODO: remove this to show all sales
             ->whereHas('event', function($query) use ($user) {
                 $query->where('user_id', $user->id);
             })
