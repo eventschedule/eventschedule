54334fdaa8f3b375d05dd37717e09660973e2a33
Hillel Coren <hillelcoren@gmail.com>
2025-01-20 14:58:17 +0200

    Tickets

--- database/migrations/2025_01_05_113546_add_stripe.php
+++ database/migrations/2025_01_05_113546_add_stripe.php
@@ -36,10 +36,12 @@ return new class extends Migration
             ->update(['ticket_currency_code' => 'USD']);
 
         Schema::table('events', function (Blueprint $table) {
+            /*
             $table->enum('payment_method', ['cash', 'stripe', 'invoiceninja'])->default('cash');
             $table->text('payment_instructions')->nullable();
             $table->text('payment_instructions_html')->nullable();
             $table->integer('expire_unpaid_tickets')->default(0);
+            */
             $table->string('ticket_currency_code')->default('USD')->change();
             $table->text('ticket_notes_html')->nullable();
         });
