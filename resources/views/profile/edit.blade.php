43be1dae96053902418d7c812372eeeb9f8c24d2
Hillel Coren <hillelcoren@gmail.com>
2025-01-21 12:27:23 +0200

    Tickets

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -14,6 +14,7 @@
                 </div>
             </div>
             @endif
+
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
