96411a5e5069ecdf2d7257c321b29a5016127732
Hillel Coren <hillelcoren@gmail.com>
2025-01-21 12:21:52 +0200

    Tickets

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,14 +7,13 @@
                 </div>
             </div>
 
-            <!--
+            @if (config('app.env') === 'local')
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-payments-form')
                 </div>
             </div>
-            -->
-
+            @endif
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
