d377ac2b639b6429d644c502b45d774c40bc58ce
Hillel Coren <hillelcoren@gmail.com>
2025-01-24 14:03:52 +0200

    Cleanup

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="p-6">
-    <img alt="Event Schedule" class="h-10" src="{{ url('images/dark-logo.png') }}">
+    <img alt="Event Schedule" class="h-14" src="{{ url('images/dark_logo.png') }}">
 </div>
