9cd4251dd332a37155bce63205f865e169671a47
Hillel Coren <hillelcoren@gmail.com>
2025-01-26 08:38:37 +0200

    Cleanup

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use App\Http\Requests\UserEmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use App\Models\Role;
+use App\Models\Ticket;
 
 class VerifyEmailController extends Controller
 {
@@ -20,6 +22,30 @@ class VerifyEmailController extends Controller
         }
 
         if ($request->user()->markEmailAsVerified()) {
+            $roles = Role::whereEmail($user->email)
+                            ->whereNull('user_id')
+                            ->get();
+
+            foreach ($roles as $role) {
+                $role->user_id = $user->id;
+                $role->save();
+
+                if ($role->markEmailAsVerified()) {
+                    event(new Verified($role));
+                }    
+
+                $user->roles()->attach($role->id, ['level' => 'owner', 'created_at' => now()]);
+            }
+
+            $tickets = Ticket::whereEmail($user->email)
+                            ->whereNull('user_id')
+                            ->get();
+
+            foreach ($tickets as $ticket) {
+                $ticket->user_id = $user->id;
+                $ticket->save();
+            }
+
             event(new Verified($request->user()));
         }
         
