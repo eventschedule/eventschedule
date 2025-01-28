69abc35c23c01a3febe53d31c9ab5bd19884e65d
Hillel Coren <hillelcoren@gmail.com>
2025-01-28 19:54:16 +0200

    Clean up

--- resources/views/ticket/sales_table.blade.php
+++ resources/views/ticket/sales_table.blade.php
@@ -72,7 +72,9 @@
 
                                         <div x-show="open" 
                                              @click.away="open = false"
-                                             class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
+                                             x-data="{ isLastRow: $el.closest('tr').nextElementSibling === null }"
+                                             :class="isLastRow ? 'bottom-full mb-2' : 'top-full mt-2'"
+                                             class="absolute right-0 z-50 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
                                              role="menu" 
                                              aria-orientation="vertical">
                                             
