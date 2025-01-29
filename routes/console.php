54ecd09a0011cd7a1ed8f35aad5f0bd48e3dc079
Hillel Coren <hillelcoren@gmail.com>
2025-01-29 18:20:37 +0200

    Clean up

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Artisan;
 
 Schedule::call(function () {
     Artisan::call('app:release_tickets');
-//})->cron('* * * * *');
-})->hourly()->appendOutputTo(storage_path('logs/scheduler.log'));
+})->cron('*/10 * * * *')->appendOutputTo(storage_path('logs/scheduler.log'));
