69e13746e10b5d147e1c6f2f3284de88d67af78f
Hillel Coren <hillelcoren@gmail.com>
2025-01-29 13:12:09 +0200

    Clean up

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -103,6 +103,29 @@
               @endif
             </div>
           @endif
+          @if ($event->venue)
+          <div
+            class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 2C7.58172 2 4 6.00258 4 10.5C4 14.9622 6.55332 19.8124 10.5371 21.6744C11.4657 22.1085 12.5343 22.1085 13.4629 21.6744C17.4467 19.8124 20 14.9622 20 10.5C20 6.00258 16.4183 2 12 2ZM12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z"
+              />
+            </svg>
+            <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode($event->venue->bestAddress()) }}" target="_blank">
+              <p class="text-sm hover:underline">
+                {{ $event->venue->shortAddress() }}
+              </p>
+            </a>
+          </div>
+          @endif
           @if ($event->isMultiDay())
           <div
             class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white"
@@ -147,7 +170,7 @@
             <p class="text-sm">{{ $event->getStartDateTime($date, true)->format('F j, Y') }}</p>
           </div>
           <div
-            class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4 {{ $event->venue ? "sm:after:content-[''] sm:after:block sm:after:absolute sm:after:right-0 sm:after:top-[50%] sm:after:translate-y-[-50%] sm:after:h-[12px] sm:after:w-[1px] sm:after:bg-white" : "" }}"
+            class="flex flex-row gap-2 items-center relative text-white fill-white sm:pr-4"
           >
             <svg
               width="24"
@@ -164,29 +187,6 @@
             <p class="text-sm">
               {{ $event->getStartEndTime($date) }}
             </p>
-          </div>
-          @endif
-          @if ($event->venue)
-          <div
-            class="flex flex-row gap-2 items-center relative text-white fill-white"
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12 2C7.58172 2 4 6.00258 4 10.5C4 14.9622 6.55332 19.8124 10.5371 21.6744C11.4657 22.1085 12.5343 22.1085 13.4629 21.6744C17.4467 19.8124 20 14.9622 20 10.5C20 6.00258 16.4183 2 12 2ZM12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z"
-              />
-            </svg>
-            <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode($event->venue->bestAddress()) }}" target="_blank">
-              <p class="text-sm hover:underline">
-                {{ $event->venue->shortAddress() }}
-              </p>
-            </a>
           </div>
           @endif
         </div>
