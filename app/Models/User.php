16c88d05f1b79192c249222b02fa30f0cbf04dc3
Hillel Coren <hillelcoren@gmail.com>
2025-01-30 13:38:14 +0200

    Clean up

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,7 +81,8 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->belongsToMany(Role::class)
                     ->withTimestamps()
                     ->withPivot('level')
-                    ->where('is_deleted', false);
+                    ->where('is_deleted', false)
+                    ->orderBy('name');
     }
     
     public function countRoles()
