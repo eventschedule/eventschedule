e6ea5936c52076676da995a43d97cea4460de7c8
Hillel Coren <hillelcoren@gmail.com>
2025-01-30 15:29:03 +0200

    Clean up

--- resources/views/role/partials/calendar.blade.php
+++ resources/views/role/partials/calendar.blade.php
@@ -190,12 +190,12 @@
                             @php
                             $canEdit = auth()->user() && auth()->user()->canEditEvent($each);
                             @endphp
-                            <li class="relative group {{ $canEdit ? 'hover:pr-8' : '' }}">
+                            <li class="relative group {{ $canEdit ? 'hover:pr-8' : '' }} hover:break-all break-words">
                                 <a href="{{ $each->getGuestUrl(isset($subdomain) ? $subdomain : '', $currentDate->format('Y-m-d')) }}"
                                     class="flex has-tooltip" data-tooltip="<b>{{ $each->name }}</b><br/>{{ $each->getVenueDisplayName() }} • {{ Carbon\Carbon::parse($each->localStartsAt())->format(isset($role) && $role->use_24_hour_time ? 'H:i' : 'g:i A') }}"
                                     onclick="event.stopPropagation();" {{ ($route != 'guest' || (isset($embed) && $embed)) ? "target='_blank'" : '' }}>
                                     <p class="flex-auto font-medium group-hover:text-[#4E81FA] text-gray-900">
-                                        <span class="{{ count($eventsMap[$currentDate->format('Y-m-d')]) == 1 ? 'line-clamp-2' : 'truncate' }} hover:underline break-words">
+                                        <span class="{{ count($eventsMap[$currentDate->format('Y-m-d')]) == 1 ? 'line-clamp-2' : 'truncate' }} hover:underline">
                                         @if (isset($subdomain) && $each->isRoleAMember($subdomain))
                                             {{ $each->getVenueDisplayName() }}
                                         @else
