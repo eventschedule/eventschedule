b088bebf00e9b9d32ce1ed8f91678163a2a5bf16
Hillel Coren <hillelcoren@gmail.com>
2025-02-13 18:00:57 +0200

    Check data

--- app/Console/Commands/CheckData.php
+++ app/Console/Commands/CheckData.php
@@ -58,18 +58,26 @@ class CheckData extends Command
 
         foreach ($events as $event) {
             if (! $event->venue && ! $event->event_url) {
-                $errors[] = 'No venue or event_url for event ' . $event->id . ': ' . $event->name;
+                $errors[] = 'No venue or event_url for event ' . $event->id . ': ' . $event->name . ' - ' . $event->getGuestUrl();
             }
 
             $data = $event->getGuestUrlData();
 
-            if (! $data['subdomain']) {
-                $errors[] = 'No subdomain for event ' . $event->id . ': ' . $event->name;
+            if (! $data['subdomain']) {                
+                $error = 'No subdomain for event ' . $event->id . ': ' . $event->name . ' - ';
+
+                foreach ($event->roles as $role) {
+                    $error .= $role->name . ' (' . $role->type . '), ';
+                }
+
+                $error = rtrim($error, ', ');
+
+                $errors[] = $error;
             }            
         }
 
         if (count($errors) > 0) {
-            $this->error("Errors found: \n");
+            $this->error("Errors found:");
             $this->info(implode("\n", $errors));
         } else {
             $this->info('No errors found');
