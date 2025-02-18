ab0472c4ecae68e157780c26d2574e359c3073ad
Hillel Coren <hillelcoren@gmail.com>
2025-02-18 22:09:38 +0200

    App setup

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Models\User;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -16,6 +17,10 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        if (! config('app.hosted') && User::count() === 0) {
+            return redirect()->route('sign_up');
+        }
+
         return view('auth.login');
     }
 
