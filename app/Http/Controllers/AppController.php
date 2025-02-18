0923530b2dacc4b969ad005dfa0b6da3dabad2b1
Hillel Coren <hillelcoren@gmail.com>
2025-02-18 19:38:45 +0200

    App update

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -12,12 +12,16 @@ class AppController extends Controller
             return redirect()->back()->with('error', 'Not authorized');
         }
 
-        if ($updater->source()->isNewVersionAvailable()) {
-            $versionAvailable = $updater->source()->getVersionAvailable();
-            $release = $updater->source()->fetch($versionAvailable);
-            $updater->source()->update($release);    
-        } else {
-            echo "No new version available.";
+        try {
+            if ($updater->source()->isNewVersionAvailable()) {
+                $versionAvailable = $updater->source()->getVersionAvailable();
+                $release = $updater->source()->fetch($versionAvailable);
+                $updater->source()->update($release);    
+            } else {
+                echo "No new version available.";
+            }                
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', __('messages.app_update_tip', ['link' => '<a href="https://github.com/eventschedule/eventschedule/releases/download/' . $versionAvailable . '/eventschedule.zip" class="hover:underline">eventschedule.zip</a>']));
         }
     }
 
