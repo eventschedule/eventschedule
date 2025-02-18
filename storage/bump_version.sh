9a82ce33ec122a8268b678a71c503c97f68da83d
Hillel Coren <hillelcoren@gmail.com>
2025-02-18 21:24:06 +0200

    App update

---
+++ storage/bump_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_version=$(grep "automatic_release_tag" .github/workflows/build.yml | grep -o "v0.9.[0-9]*" | cut -d "." -f3)
+new_version=$((current_version+1))
+date_today=$(date +%F)
+
+echo "Bump version... $current_version => $new_version"
+
+sed -i -e "s/automatic_release_tag: \"v0.9.$current_version\"/automatic_release_tag: \"v0.9.$new_version\"/g" .github/workflows/build.yml
+sed -i -e "s/'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.$current_version')/'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.$new_version')/g" config/self-update.php
