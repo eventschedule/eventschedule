726b93519b73b88631be9b2ae3e1dbe56bdc9b66
Hillel Coren <hillelcoren@gmail.com>
2025-02-18 21:26:00 +0200

    Version bump script

--- storage/bump_version.sh
+++ storage/bump_version.sh
@@ -8,3 +8,6 @@ echo "Bump version... $current_version => $new_version"
 
 sed -i -e "s/automatic_release_tag: \"v0.9.$current_version\"/automatic_release_tag: \"v0.9.$new_version\"/g" .github/workflows/build.yml
 sed -i -e "s/'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.$current_version')/'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.$new_version')/g" config/self-update.php
+
+rm .github/workflows/build.yml-e
+rm config/self-update.php-e
