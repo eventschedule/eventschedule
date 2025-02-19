ea3480452c2f301edd50d8513286d5ad0a904b18
Hillel Coren <hillelcoren@gmail.com>
2025-02-19 13:36:42 +0200

    Fixes for tests

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             ]);
         }
 
-        if (! config('app.hosted') && ! config('app.url') && ! app()->runningInConsole()) {
+        if (! config('app.hosted') && ! config('app.url')) {
             $request->validate([
                 'database_host' => ['required', 'string', 'max:255'],
                 'database_port' => ['required', 'string', 'max:255'],
