5428f66c7828b9effb6242338a03b4199f8be9c2
Hillel Coren <hillelcoren@gmail.com>
2025-02-19 15:42:46 +0200

    Fix for error reporting

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -189,7 +189,7 @@
         <div class="mt-4">
             <div class="relative flex items-start">
                 <div class="flex h-6 items-center">
-                    <input id="report_errors" name="report_errors" type="checkbox"
+                    <input id="report_errors" name="report_errors" type="checkbox" value="1"
                         class="h-4 w-4 rounded border-gray-300 text-[#4E81FA] focus:ring-[#4E81FA]">
                 </div>
                 <div class="ml-3 text-sm leading-6">
