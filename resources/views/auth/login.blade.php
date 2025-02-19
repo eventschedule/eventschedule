b0b19dd7d6d619a6f200b5c0e82ce4c31de8caef
Hillel Coren <hillelcoren@gmail.com>
2025-02-19 14:45:14 +0200

    Hide create account link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,9 +40,13 @@
         </div>
 
         <div class="flex items-center justify-between mt-4">
+            @if (config('app.hosted'))
             <a class="hover:underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#4E81FA] dark:focus:ring-offset-gray-800" href="{{ route('sign_up') }}">
                 {{ __('messages.create_new_account') }}
             </a>
+            @else
+            <div></div>
+            @endif
 
             <x-primary-button class="ml-4">
                 {{ __('messages.log_in') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -196,10 +196,14 @@
         </div>
 
         <div class="flex items-center justify-between mt-6">
+            @if (config('app.hosted'))
             <a class="hover:underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#4E81FA] dark:focus:ring-offset-gray-800"
                 href="{{ route('login') }}">
                 {{ __('messages.already_registered') }}
             </a>
+            @else
+            <div></div>
+            @endif
 
             <x-primary-button class="ml-4">
                 {{ __('messages.register') }}
