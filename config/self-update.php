1f931e461fd4abf0d467ae57731514e5f004b5bb
Hillel Coren <hillelcoren@gmail.com>
2025-02-19 15:39:42 +0200

    Update version

--- config/self-update.php
+++ config/self-update.php
@@ -24,7 +24,7 @@ return [
     |
     */
 
-    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.14'),
+    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', 'v0.9.15'),
 
     /*
     |--------------------------------------------------------------------------
