178515d05acd992e5dff58f9e1db378307410ac7
Hillel Coren <hillelcoren@gmail.com>
2025-03-09 14:08:06 +0200

    Support translated schedules

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -31,7 +31,7 @@
           <div style="font-family: sans-serif" class="mt-8 relative inline-block text-left">
           @if ($event->canSellTickets() || $event->registration_url)
             @if (request()->get('tickets') !== 'true')
-              <a href="{{ $event->registration_url ? $event->registration_url : request()->fullUrlWithQuery(['tickets' => 'true']) }}">
+              <a href="{{ $event->registration_url ? $event->registration_url : request()->fullUrlWithQuery(['tickets' => 'true']) }}" {{ $event->registration_url ? 'target="_blank"' : '' }}>
                   <button type="button" 
                         class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-6 py-3 text-lg font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                     {{ __('messages.buy_tickets') }}
