a905071bcad8823d112dd3e0c138bdac51f8c978
Hillel Coren <hillelcoren@gmail.com>
2025-03-10 10:43:28 +0200

    Support translated schedules

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="p-6">
-    <img class="h-12 w-auto dark:hidden" src="{{ url('images/dark_logo.png') }}"/>
-    <img class="h-12 w-auto hidden dark:block" src="{{ url('images/light_logo.png') }}"/>
+    <img class="h-10 md:h-12 w-auto dark:hidden" src="{{ url('images/dark_logo.png') }}"/>
+    <img class="h-10 md:h-12 w-auto hidden dark:block" src="{{ url('images/light_logo.png') }}"/>
 </div>
