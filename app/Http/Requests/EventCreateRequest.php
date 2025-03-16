4371693654bafa8ba1962d960fcca80a1e995199
Hillel Coren <hillelcoren@gmail.com>
2025-03-16 16:03:15 +0200

    Code cleanup

--- app/Http/Requests/EventCreateRequest.php
+++ app/Http/Requests/EventCreateRequest.php
@@ -5,7 +5,6 @@ namespace App\Http\Requests;
 use App\Models\Role;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use App\Rules\NoFakeEmail;
 
 class EventCreateRequest extends FormRequest
 {
