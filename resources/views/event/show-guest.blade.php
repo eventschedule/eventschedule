3d539f7acce0d945757675de2621a507cf1c417d
Hillel Coren <hillelcoren@gmail.com>
2025-03-23 08:53:44 +0200

    Hide talent in event CP if no info set

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -216,52 +216,6 @@
           </div>
         </div>
         @else
-        <!--
-        <div>
-          <h3 class="text-[32px] leading-snug text-black mb-6">
-            {{ __('messages.date_and_time') }}
-          </h3>
-          <div class="flex flex-col gap-3">
-            <div
-              class="flex flex-row gap-2 items-center relative text-[#33383C] fill-[#33383C]"
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7.75 2.5C7.75 2.08579 7.41421 1.75 7 1.75C6.58579 1.75 6.25 2.08579 6.25 2.5V4.07926C4.81067 4.19451 3.86577 4.47737 3.17157 5.17157C2.47737 5.86577 2.19451 6.81067 2.07926 8.25H21.9207C21.8055 6.81067 21.5226 5.86577 20.8284 5.17157C20.1342 4.47737 19.1893 4.19451 17.75 4.07926V2.5C17.75 2.08579 17.4142 1.75 17 1.75C16.5858 1.75 16.25 2.08579 16.25 2.5V4.0129C15.5847 4 14.839 4 14 4H10C9.16097 4 8.41527 4 7.75 4.0129V2.5Z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2 12C2 11.161 2 10.4153 2.0129 9.75H21.9871C22 10.4153 22 11.161 22 12V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V12ZM17 14C17.5523 14 18 13.5523 18 13C18 12.4477 17.5523 12 17 12C16.4477 12 16 12.4477 16 13C16 13.5523 16.4477 14 17 14ZM17 18C17.5523 18 18 17.5523 18 17C18 16.4477 17.5523 16 17 16C16.4477 16 16 16.4477 16 17C16 17.5523 16.4477 18 17 18ZM13 13C13 13.5523 12.5523 14 12 14C11.4477 14 11 13.5523 11 13C11 12.4477 11.4477 12 12 12C12.5523 12 13 12.4477 13 13ZM13 17C13 17.5523 12.5523 18 12 18C11.4477 18 11 17.5523 11 17C11 16.4477 11.4477 16 12 16C12.5523 16 13 16.4477 13 17ZM7 14C7.55228 14 8 13.5523 8 13C8 12.4477 7.55228 12 7 12C6.44772 12 6 12.4477 6 13C6 13.5523 6.44772 14 7 14ZM7 18C7.55228 18 8 17.5523 8 17C8 16.4477 7.55228 16 7 16C6.44772 16 6 16.4477 6 17C6 17.5523 6.44772 18 7 18Z"
-                />
-              </svg>
-              <p class="text-sm">{{ $event->getStartDateTime($date, true)->format('F j, Y') }}</p>
-            </div>
-            <div
-              class="flex flex-row gap-2 items-center relative text-[#33383C] fill-[#33383C]"
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM12.75 8C12.75 7.58579 12.4142 7.25 12 7.25C11.5858 7.25 11.25 7.58579 11.25 8V12C11.25 12.1989 11.329 12.3897 11.4697 12.5303L13.9697 15.0303C14.2626 15.3232 14.7374 15.3232 15.0303 15.0303C15.3232 14.7374 15.3232 14.2626 15.0303 13.9697L12.75 11.6893V8Z"
-                />
-              </svg>
-              <p class="text-sm">{{ $event->getStartEndTime($date) }}</p>
-            </div>
-          </div>
-        </div>
-        -->
         <div>
         @if ($translation ? $translation->description_translated : $event->translatedDescription())
           <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6 flex flex-col gap-4 {{ $role->isRtl() || ($translation && $translation->role->isRtl()) ? 'rtl' : '' }}">
@@ -275,12 +229,11 @@
             </div>
           </div>
           @endif
-          @if ($event->flyer_image_url && $event->members()->count() != 1)
-          <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6 flex flex-col gap-4">
-            <img src="{{ $event->flyer_image_url }}" class="block"/>
-          </div>
-          @endif
-          @foreach ($event->members() as $each)          
+          
+          @foreach ($event->members() as $each)
+            @if (! $each->isClaimed() && ! $each->getFirstVideoUrl())
+              @continue       
+            @endif
           <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6 flex flex-col gap-4" 
             style="font-family: {{ $each->isClaimed() ? $each->font_family : $otherRole->font_family }}, sans-serif;"
           >
@@ -372,13 +325,11 @@
 
           @endforeach
 
-          @if ($event->flyer_image_url && $event->members()->count() == 1)
+          @if ($event->flyer_image_url)
           <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6 flex flex-col gap-4">
             <img src="{{ $event->flyer_image_url }}" class="block"/>
           </div>
           @endif
-
-
 
           <div class="bg-[#F5F9FE] rounded-2xl p-8 mb-6">
             @include('role/partials/calendar', ['route' => 'guest', 'tab' => ''])
