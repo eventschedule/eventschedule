25816ecb144279a882d07a202abdaf6fa61e5204
Hillel Coren <hillelcoren@gmail.com>
2025-04-08 17:47:46 +0300

    Working on the API...

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ScheduleController;
+use App\Http\Controllers\Api\ApiScheduleController;
 use App\Http\Controllers\Api\ApiSettingsController;
 use App\Http\Middleware\ApiAuthentication;
 
 Route::middleware([ApiAuthentication::class])->prefix('api')->group(function () {
     Route::get('/schedules', [ScheduleController::class, 'index']);
+    Route::post('/schedules', [ScheduleController::class, 'store']);
+    Route::put('/schedules/{schedule}', [ScheduleController::class, 'update']);
+    
     Route::get('/schedules/{schedule}/events', [ScheduleController::class, 'events']);
     Route::post('/schedules/{schedule}/events', [ScheduleController::class, 'storeEvent']);
 }); 
