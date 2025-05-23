0ff6b1ee5006e56cd5c712e47238ff5d518d3cdd
Hillel Coren <hillelcoren@gmail.com>
2025-05-23 13:27:41 +0300

    Fix for webhooks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'stripe/webhook',
+            'invoiceninja/webhook/*',
+        ]);
+        
         $middleware->web(append: [
             SetUserLanguage::class,
             EnsureEmailIsVerified::class,
