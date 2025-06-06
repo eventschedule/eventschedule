b1c280a33487da1801ef124d2fba1a4cf67c3f43
Hillel Coren <hillelcoren@gmail.com>
2025-06-06 16:11:55 +0300

    Groups

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     protected $fillable = [
-        'name', 'name_en', 'slug', 'role_id',
+        'name', 
+        'name_en', 
+        'slug', 
     ];
 
     public function role()
