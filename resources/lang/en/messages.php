436bc6e67a51c1f339a6620519f907e821b7e425
Hillel Coren <hillelcoren@gmail.com>
2025-06-24 18:07:02 +0300

    Support payment URLs

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -486,7 +486,7 @@ return [
     'iframe_code' => 'Iframe Code',
     'close' => 'Close',
     'payment_url' => 'Payment Link',
-    'payment_url_help' => 'Configure a custom payment URL that customers will be redirected to for payment processing.',
+    'payment_url_help' => 'Configure a custom payment URL that customers will be redirected to.',
     'payment_url_connected' => 'Payment link connected successfully!',
     'payment_url_unlinked' => 'Payment link unlinked successfully!',
 ];
