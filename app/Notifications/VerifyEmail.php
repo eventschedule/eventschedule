65a731a8938e757daac055ec3322f2c6eb8ab768
Hillel Coren <hillelcoren@gmail.com>
2025-06-25 16:14:41 +0300

    Add unsubscribe email header

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -56,7 +56,7 @@ class VerifyEmail extends BaseVerifyEmail
             public function content(): Content
             {
                 return new Content(
-                    markdown: 'vendor.notifications.email',
+                    view: 'vendor.notifications.email',
                     with: [
                         'greeting' => 'Hello!',
                         'introLines' => ['Please click the button below to verify your email address.'],
@@ -64,7 +64,7 @@ class VerifyEmail extends BaseVerifyEmail
                         'actionUrl' => $this->verificationUrl,
                         'displayableActionUrl' => $this->verificationUrl,
                         'outroLines' => [],
-                        'salutation' => "Regards,\nThe Event Schedule team",
+                        'salutation' => 'Regards,<br>The Event Schedule team',
                         'level' => 'primary',
                     ],
                 );
