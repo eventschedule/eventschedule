64140e4ef7edbfdc02dfdeefacbd3148a2a918fe
Hillel Coren <hillelcoren@gmail.com>
2025-07-21 23:00:02 +0300

    Update model

--- app/Utils/GeminiUtils.php
+++ app/Utils/GeminiUtils.php
@@ -14,7 +14,7 @@ class GeminiUtils
     private static function sendRequest($prompt, $imageData = null)
     {
         $apiKey = config('services.google.gemini_key');
-        $model = 'gemini-2.0-flash';
+        $model = 'gemini-2.5-flash';
         $url = "https://generativelanguage.googleapis.com/v1beta/models/{$model}:generateContent?key=" . $apiKey;
         
         $data = [
