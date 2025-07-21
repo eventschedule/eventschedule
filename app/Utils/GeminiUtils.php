5ee3dbf068bc0db16d74a49687acaf6f6129f9ad
Hillel Coren <hillelcoren@gmail.com>
2025-07-21 14:01:59 +0300

    Curator import

--- app/Utils/GeminiUtils.php
+++ app/Utils/GeminiUtils.php
@@ -167,7 +167,7 @@ class GeminiUtils
             'event_name_en' => 'only if the event_name is not English',
             'event_date_time' => 'YYYY-MM-DD HH:MM',
             'event_duration' => 'in hours',
-            'event_details' => '',
+            'event_details' => 'In original language, using markdown formatting with as much accurate text as possible describing the event',
             'event_address' => 'Just the street address, do not include the city or state',
             'event_address_en' => 'English translation, only if the event address is not English',
             'event_city' => '',
