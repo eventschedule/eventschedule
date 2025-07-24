5485cab217d2a11a06d8b8e711564d29497ccad4
Hillel Coren <hillelcoren@gmail.com>
2025-07-24 21:53:22 +0300

    CP adjustments

--- resources/views/event/show-guest.blade.php
+++ resources/views/event/show-guest.blade.php
@@ -552,14 +552,17 @@
         @endif
 
         <div class="p-6 rounded-xl flex flex-col gap-6 bg-[#F5F9FE] {{ $role->isRtl() ? 'rtl' : '' }}">
-          <h4 class="text-[#151B26] text-[24px] leading-snug font-semibold">
+          <h4 class="text-[24px] leading-snug font-semibold text-gray-900">
             {{ __('messages.create_your_own_event_schedule') }}
           </h4>
           <a href="https://www.eventschedule.com" target="_blank">
             <button
               type="button"
               name="login"
-              class="inline-flex items-center justify-center rounded-xl text-base duration-300 bg-transparent border-[1px] border-[#4E81FA] hover:border-[#1A48B3] hover:bg-[#1A48B3] text-[#4E81FA] hover:text-white py-4 px-8 hover:opacity-90 disabled:cursor-not-allowed disabled:opacity-90"
+              class="inline-flex items-center justify-center rounded-xl text-base duration-300 bg-transparent border-[1px] py-4 px-8 hover:opacity-90 disabled:cursor-not-allowed disabled:opacity-90"
+              style="border-color: {{ $role->accent_color ?? '#4E81FA' }}; color: {{ $role->accent_color ?? '#4E81FA' }};"
+              onmouseover="this.style.backgroundColor='{{ $role->accent_color ?? '#4E81FA' }}'; this.style.color='white';"
+              onmouseout="this.style.backgroundColor='transparent'; this.style.color='{{ $role->accent_color ?? '#4E81FA' }}';"
             >
               {{ __('messages.create_schedule') }}
             </button>
