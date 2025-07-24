7ef9bc3864dbb5745d85702d578d90f830d67f12
Hillel Coren <hillelcoren@gmail.com>
2025-07-24 21:32:13 +0300

    CP adjustments

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -87,7 +87,7 @@
                                 <select name="featured_image" id="featured_image"
                                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                     <option value="">No featured image</option>
-                                    @foreach(\App\Models\BlogPost::getAvailableHeaderImages() as $image => $description)
+                                    @foreach(\App\Models\BlogPost::getAvailableHeaderImages(! $blogPost->exists) as $image => $description)
                                         <option value="{{ $image }}" {{ old('featured_image', $blogPost->featured_image) == $image ? 'selected' : '' }}>
                                             {{ $description }}
                                         </option>
