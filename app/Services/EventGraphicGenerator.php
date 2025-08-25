fb5fee22dfb97c9ba9c69c182da5bac693587793
Hillel Coren <hillelcoren@gmail.com>
2025-08-25 16:50:27 +0300

    Events graphic

--- app/Services/EventGraphicGenerator.php
+++ app/Services/EventGraphicGenerator.php
@@ -9,34 +9,5 @@ use Illuminate\Support\Collection;
 
 class EventGraphicGenerator
 {
-    private AbstractEventDesign $design;
 
-    public function __construct(Role $role, Collection $events, string $design = 'modern')
-    {
-        $this->design = $this->createDesign($design, $role, $events);
-    }
-
-    public function generate(): string
-    {
-        return $this->design->generate();
-    }
-
-    private function createDesign(string $design, Role $role, Collection $events): AbstractEventDesign
-    {
-        return match($design) {
-            'modern' => new ModernDesign($role, $events),
-            'minimal' => new MinimalDesign($role, $events),
-            default => new ModernDesign($role, $events),
-        };
-    }
-
-    public function getWidth(): int
-    {
-        return $this->design->getWidth();
-    }
-
-    public function getHeight(): int
-    {
-        return $this->design->getHeight();
-    }
 }
