8d7a9862be471983377c3f750fc8efb628a0d657
Hillel Coren <hillelcoren@gmail.com>
2025-08-26 18:24:53 +0300

    Check pro for graphics feature

--- resources/views/role/show-admin.blade.php
+++ resources/views/role/show-admin.blade.php
@@ -258,7 +258,7 @@
                                     {{ __('messages.embed_schedule') }}
                                 </div>
                             </a>
-                            <a href="{{ route('event.generate_graphic', ['subdomain' => $role->subdomain]) }}" class="group flex items-center px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">
+                            <a href="#" onclick="handleEventsGraphicClick()" class="group flex items-center px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                     <path d="M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19M8.5,13.5L11,16.5L14.5,12L19,18H5L8.5,13.5Z" />
                                 </svg>
@@ -383,5 +383,16 @@
     @elseif ($tab == 'plan')
     @include('role.show-admin-plan')
     @endif
+
+<script>
+function handleEventsGraphicClick() {
+    @if (!$role->isPro())
+        alert('{{ __("messages.requires_pro_plan") }}');
+        return false;
+    @else
+        window.location.href = '{{ route("event.generate_graphic", ["subdomain" => $role->subdomain]) }}';
+    @endif
+}
+</script>
 
 </x-app-admin-layout>
