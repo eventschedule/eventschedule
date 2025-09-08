6f40217484a75c9a775ab1bfa01f9581f08b64ec
Hillel Coren <hillelcoren@gmail.com>
2025-09-08 18:02:34 +0300

    Events text

--- resources/views/graphic/show.blade.php
+++ resources/views/graphic/show.blade.php
@@ -22,6 +22,10 @@
                 });
             }
             
+            
+            
+            
+            
             function downloadImage() {
                 if (!graphicData) return;
                 
@@ -138,17 +142,6 @@
                 <!-- Left Column - Image -->
                 <div class="space-y-4 flex flex-col">
                     <div class="flex flex-col sm:flex-row gap-3">
-                        <button 
-                            onclick="copyToClipboard(graphicData.download_url, 'copyImageBtn')"
-                            id="copyImageBtn"
-                            class="inline-flex items-center justify-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md transition-colors duration-200"
-                        >
-                            <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                            </svg>
-                            {{ __('messages.copy_image') }}
-                        </button>
-                        
                         <button 
                             id="downloadBtn"
                             onclick="downloadImage()"
