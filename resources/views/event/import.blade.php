121b3162eaf5780a20f4037b73f6fbd171981091
Hillel Coren <hillelcoren@gmail.com>
2025-09-17 13:23:19 +0300

    Show create account

--- resources/views/event/import.blade.php
+++ resources/views/event/import.blade.php
@@ -333,6 +333,13 @@
                             <label for="create_account_@{{ idx }}" class="ml-2 text-sm font-medium text-gray-700 dark:text-gray-300">
                                 {{ __('messages.create_account') }}
                             </label>
+                            <svg class="w-4 h-4 ml-1 text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 cursor-help" 
+                                 title="{{ __('messages.create_account_benefits') }}"
+                                 fill="currentColor" 
+                                 viewBox="0 0 20 20" 
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+                            </svg>
                         </div>
                         
                         <div v-if="createAccount" class="space-y-4">
