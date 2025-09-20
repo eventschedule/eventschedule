d319aceab2c200210dd681aaab36959bf2a87ad7
Hillel Coren <hillelcoren@gmail.com>
2025-09-21 00:58:48 +0300

    Google Calendar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
+            'google-calendar/webhook',
             'stripe/webhook',
             'invoiceninja/webhook/*',
         ]);
