903075092710399de665566e8f0f867e32fe501e
Hillel Coren <hillelcoren@gmail.com>
2025-09-21 12:55:16 +0300

    Google Calendar

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -69,6 +69,15 @@ class Role extends Model implements MustVerifyEmail
     ];
 
     /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'google_webhook_expires_at' => 'datetime',
+    ];
+
+    /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
