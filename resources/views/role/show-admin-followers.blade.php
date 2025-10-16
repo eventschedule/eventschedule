40ef198730833f9b72e878c83a792f3d5b0b2eb1
Hillel Coren <hillelcoren@gmail.com>
2025-10-16 15:55:17 +0300

    Show follower schedule

--- resources/views/role/show-admin-followers.blade.php
+++ resources/views/role/show-admin-followers.blade.php
@@ -96,13 +96,13 @@
                 {{ __('messages.previous') }}
             </span>
         @else
-            <a href="{{ $followers->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+            <a href="{{ $followersWithRoles->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                 {{ __('messages.previous') }}
             </a>
         @endif
 
         @if ($followersWithRoles->hasMorePages())
-            <a href="{{ $followers->nextPageUrl() }}" class="ml-3 relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+            <a href="{{ $followersWithRoles->nextPageUrl() }}" class="ml-3 relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                 {{ __('messages.next') }}
             </a>
         @else
