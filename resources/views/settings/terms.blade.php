c7679d3471aa999d9103400cbf17f6600a2ff032
dfiore1230 <dfiore1230@users.noreply.github.com>
2025-11-18 08:03:33 -0500

    Improve settings page headers

--- resources/views/settings/terms.blade.php
+++ resources/views/settings/terms.blade.php
@@ -11,7 +11,6 @@
                             ]"
                             class="text-xs text-gray-500 dark:text-gray-400"
                         />
-                        <p class="text-sm font-medium text-indigo-600">{{ __('messages.settings') }}</p>
                         <h1 class="mt-2 text-2xl font-semibold text-gray-900 dark:text-white">
                             {{ __('messages.terms_settings') }}
                         </h1>
--- resources/views/settings/updates.blade.php
+++ resources/views/settings/updates.blade.php
@@ -59,7 +59,6 @@
                             ]"
                             class="text-xs text-gray-500 dark:text-gray-400"
                         />
-                        <p class="text-sm font-medium text-indigo-600">{{ __('messages.settings') }}</p>
                         <h1 class="mt-2 text-2xl font-semibold text-gray-900 dark:text-white">
                             {{ __('messages.update_settings') }}
                         </h1>
