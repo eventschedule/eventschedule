3974c9e8fdcd5bcee73c555d07a205cacd3ff3ef
Hillel Coren <hillelcoren@gmail.com>
2025-12-26 14:17:44 +0200

    Layout improvments

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-admin-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto space-y-6">
+        <div class="mx-auto space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
