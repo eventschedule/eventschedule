a3d0c418b48e3fbff47d8645873edb994e8d077a
Hillel Coren <hillelcoren@gmail.com>
2025-12-27 19:49:34 +0200

    Fix tests

--- tests/Browser/Traits/AccountSetupTrait.php
+++ tests/Browser/Traits/AccountSetupTrait.php
@@ -30,7 +30,7 @@ trait AccountSetupTrait
     protected function createTestVenue(Browser $browser, string $name = 'Venue', string $address = '123 Test St'): void
     {
         $browser->visit('/new/venue')
-                ->waitForText('New Venue', 5)
+                ->waitForText('New Schedule', 5)
                 ->clear('name')
                 ->type('name', $name)
                 ->pause(1000)
@@ -47,7 +47,7 @@ trait AccountSetupTrait
     protected function createTestTalent(Browser $browser, string $name = 'Talent'): void
     {
         $browser->visit('/new/talent')
-                ->waitForText('New Talent', 5)
+                ->waitForText('New Schedule', 5)
                 ->clear('name')
                 ->type('name', $name)
                 ->pause(1000)
@@ -63,7 +63,7 @@ trait AccountSetupTrait
     protected function createTestCurator(Browser $browser, string $name = 'Curator'): void
     {
         $browser->visit('/new/curator')
-                ->waitForText('New Curator', 5)
+                ->waitForText('New Schedule', 5)
                 ->clear('name')
                 ->type('name', $name)
                 ->pause(1000)
