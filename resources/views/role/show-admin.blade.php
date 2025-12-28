b122a4d4cdf1d5d72b42092eafc1c83bc91acc7f
Hillel Coren <hillelcoren@gmail.com>
2025-12-28 12:30:11 +0200

    Fix admin actions

--- resources/views/role/show-admin.blade.php
+++ resources/views/role/show-admin.blade.php
@@ -241,7 +241,6 @@
                                     </div>
                                 </a>
                             </div>
-                            @if ($tab == 'schedule')
                             <a href="{{ route('event.show_import', ['subdomain' => $role->subdomain]) }}" class="group flex items-center px-5 py-3 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:bg-gray-100 dark:focus:bg-gray-700 focus:outline-none transition-colors" role="menuitem" tabindex="0">
                                 <svg class="mr-3 h-5 w-5 text-gray-400 dark:text-gray-500 group-hover:text-gray-500 dark:group-hover:text-gray-400" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                     <path d="M14,12L10,8V11H2V13H10V16M20,18V6C20,4.89 19.1,4 18,4H6A2,2 0 0,0 4,6V9H6V6H18V18H6V15H4V18A2,2 0 0,0 6,20H18A2,2 0 0,0 20,18Z" />
@@ -268,7 +267,6 @@
                                     {{ __('messages.events_graphic') }}
                                 </div>
                             </a>
-                            @if ($role->email_verified_at)
                             <a href="#" onclick="openEmbedModal()" class="group flex items-center px-5 py-3 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:bg-gray-100 dark:focus:bg-gray-700 focus:outline-none transition-colors" role="menuitem" tabindex="0">
                                 <svg class="mr-3 h-5 w-5 text-gray-400 dark:text-gray-500 group-hover:text-gray-500 dark:group-hover:text-gray-400" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                     <path d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
@@ -277,8 +275,6 @@
                                     {{ __('messages.embed_schedule') }}
                                 </div>
                             </a>
-                            @endif
-                            @endif
                             @if ($role->exists && $role->user_id == auth()->user()->id)
                             <div class="py-2" role="none">
                                 <div class="border-t border-gray-100 dark:border-gray-700"></div>
