3acbdaa97658788a58111c10d0dedb768031fb0c
Hillel Coren <hillelcoren@gmail.com>
2026-02-16 14:24:24 +0200

    Marketing fixes

--- resources/views/role/show-guest.blade.php
+++ resources/views/role/show-guest.blade.php
@@ -207,7 +207,7 @@
                     <div class="custom-content [&>*:first-child]:mt-0">
                       {!! \App\Utils\UrlUtils::convertUrlsToLinks($role->translatedDescription()) !!}
                     </div>
-                    <button @click="expanded = false; window.scrollTo({ top: 0, behavior: 'smooth' })" class="text-blue-600 dark:text-blue-400 hover:underline whitespace-nowrap mt-1">
+                    <button @click="expanded = false; $nextTick(() => window.scrollTo({ top: 0, behavior: 'smooth' }))" class="text-blue-600 dark:text-blue-400 hover:underline whitespace-nowrap mt-1">
                       {{ __('messages.show_less') }}
                     </button>
                   </div>
@@ -387,7 +387,7 @@
                   <div class="custom-content [&>*:first-child]:mt-0">
                     {!! \App\Utils\UrlUtils::convertUrlsToLinks($role->translatedDescription()) !!}
                   </div>
-                  <button @click="expanded = false; window.scrollTo({ top: 0, behavior: 'smooth' })" class="text-blue-600 dark:text-blue-400 hover:underline whitespace-nowrap mt-1">
+                  <button @click="expanded = false; $nextTick(() => window.scrollTo({ top: 0, behavior: 'smooth' }))" class="text-blue-600 dark:text-blue-400 hover:underline whitespace-nowrap mt-1">
                     {{ __('messages.show_less') }}
                   </button>
                 </div>
@@ -670,7 +670,7 @@
         @endphp
         @if ($videoCount > 0)
           <div
-              class="bg-white/95 dark:bg-gray-900/95 backdrop-blur-sm sm:rounded-2xl px-6 lg:px-16 py-6 flex flex-col gap-6 mb-6 transition-[max-width] duration-300 ease-in-out mx-auto" data-view-width style="max-width: {{ ($role->event_layout ?? 'calendar') === 'list' ? '56rem' : '200rem' }}"
+              class="bg-white/95 dark:bg-gray-900/95 backdrop-blur-sm rounded-2xl px-6 lg:px-16 py-6 flex flex-col gap-6 mb-6 transition-[max-width] duration-300 ease-in-out mx-auto" data-view-width style="max-width: {{ ($role->event_layout ?? 'calendar') === 'list' ? '56rem' : '200rem' }}"
             >
               <div class="grid grid-cols-1 md:grid-cols-{{ $gridCols }} gap-8">
               @foreach ($videoLinks as $link)
