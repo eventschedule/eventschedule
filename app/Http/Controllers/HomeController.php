846bc66837f59bb7040683993edb8064351e33c7
Hillel Coren <hillelcoren@gmail.com>
2026-04-12 08:05:43 +0300

    selfhosted docker compose - exception #67

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -194,6 +194,11 @@ class HomeController extends Controller
 
         $canCreateSchedule = ! config('app.hosted') || $user->roles()->where('is_deleted', false)->count() < 50;
 
+        $allRoles = app('userRoles');
+        $schedules = $allRoles->where('type', 'talent')->whereIn('pivot.level', ['owner', 'admin', 'viewer']);
+        $venues = $allRoles->where('type', 'venue')->whereIn('pivot.level', ['owner', 'admin', 'viewer']);
+        $curators = $allRoles->where('type', 'curator')->whereIn('pivot.level', ['owner', 'admin', 'viewer']);
+
         return view('home', compact(
             'events',
             'month',
@@ -216,6 +221,9 @@ class HomeController extends Controller
             'boostCampaigns',
             'trafficSources',
             'canCreateSchedule',
+            'schedules',
+            'venues',
+            'curators',
         ));
     }
 
